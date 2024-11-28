c57bbe8abef2e70d3249ee405f90cc20d263a13e
SantiMar00 <santimarinio2000@gmail.com>
2024-11-28 13:04:03 -0300

    add java-ejercicio-1

---
+++ 2. java/java-ejercicio-1/Main.java
@@ -0,0 +1,63 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        String ciudades[] = new String[10];
+        ciudades[0] = "Londres";
+        ciudades[1] = "Madrid";
+        ciudades[2] = "Nueva York";
+        ciudades[3] = "Buenos Aires";
+        ciudades[4] = "Asunción";
+        ciudades[5] = "São Paulo";
+        ciudades[6] = "Lima";
+        ciudades[7] = "Santiago de Chile";
+        ciudades[8] = "Lisboa";
+        ciudades[9] = "Tokio";
+
+        int temperaturas[][] = new int[10][2];
+        temperaturas[0][0] = -2;
+        temperaturas[0][1] = 33;
+        temperaturas[1][0] = -3;
+        temperaturas[1][1] = 32;
+        temperaturas[2][0] = -8;
+        temperaturas[2][1] = 27;
+        temperaturas[3][0] = 4;
+        temperaturas[3][1] = 37;
+        temperaturas[4][0] = 6;
+        temperaturas[4][1] = 42;
+        temperaturas[5][0] = 5;
+        temperaturas[5][1] = 43;
+        temperaturas[6][0] = 0;
+        temperaturas[6][1] = 39;
+        temperaturas[7][0] = -7;
+        temperaturas[7][1] = 26;
+        temperaturas[8][0] = -1;
+        temperaturas[8][1] = 31;
+        temperaturas[9][0] = -10;
+        temperaturas[9][1] = 35;
+
+        int tempMin = temperaturas[0][0];
+        int tempMax = temperaturas[0][1];
+        String ciudadMin = ciudades[0];
+        String ciudadMax = ciudades[0];
+
+        for (int i = 0; i < temperaturas.length; i++) {
+            if (temperaturas[i][0] < tempMin) {
+                tempMin = temperaturas[i][0];
+                ciudadMin = ciudades[i];
+            }
+
+            if (temperaturas[i][1] > tempMax) {
+                tempMax = temperaturas[i][1];
+                ciudadMax = ciudades[i];
+            }
+        }
+
+        System.out.println("La menor temperatura fue de " + tempMin + " para " + ciudadMin);
+        System.out.println("La mayor temperatura fue de " + tempMax + " para " + ciudadMax);
+
+    }
+}
