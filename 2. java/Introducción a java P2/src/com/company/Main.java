29fab15310fe770915f926952314588a00e8af0d
vilo0812 <gabriel.viloria0812@gmail.com>
2024-11-28 16:42:44 -0500

    ejercicio introducción java p2

---
+++ 2. java/Introducción a java P2/src/com/company/Main.java
@@ -0,0 +1,190 @@
+package com.company;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+public class Main {
+
+    public static void main(String[] args) {
+        //region Ejercicio#1
+        /*double sueldoBase = 21000; //monto de ejemplo
+        String dni = "12345678"; //dni de ejemplo
+        double sueldoConAumento;
+
+        if (sueldoBase <= 20000) {
+            sueldoConAumento = sueldoBase + ((sueldoBase * 20) / 100);
+        }
+        else {
+            if (sueldoBase >= 20000 && sueldoBase <= 45000){
+                sueldoConAumento = sueldoBase + ((sueldoBase * 10) / 100);
+            }
+            else {
+                sueldoConAumento = sueldoBase + ((sueldoBase * 5) / 100);
+            }
+        }*/
+
+        //System.out.println ("El nuevo sueldo del empleado es de: " + sueldoConAumento);
+        //endregion
+
+        //region Ejercicio#2
+        /*int serviciosCli[] = new int[7]; //vector de 7 posiciones con tipos de servicios
+        serviciosCli[0] =1;
+        serviciosCli[1] =1;
+        serviciosCli[2] =2;
+        serviciosCli[3] =2;
+        serviciosCli[4] =2;
+        serviciosCli[5] =1;
+        serviciosCli[6] =2;
+        double totalFactura;
+        double precioFijo = 1500;
+        double servicioPatrullaje = 700;
+        for (int i=0;i <= (serviciosCli.length - 1);i++) {
+            if (serviciosCli[i] == 1) {
+                totalFactura = precioFijo;
+                System.out.println("El tipo de servicio es: " + serviciosCli[i]);
+                System.out.println("El monto de la factura es de: " + totalFactura);
+            } else {
+                totalFactura = precioFijo + servicioPatrullaje;
+                System.out.println("El tipo de servicio es: " + serviciosCli[i]);
+                System.out.println("El monto de la factura es de: " + totalFactura);
+            }
+        }*/
+        //endregions
+
+        //region Ejercicio en VIVO
+        //Variables
+        boolean inscripcionCerrada = false;
+        Scanner console = new Scanner(System.in);
+        //List<String> listaCategoria = new ArrayList<String>();
+        List<List<String>> listaCategorias = new ArrayList<List<String>>();
+        Map<Integer, Map<String, Object>> participantes = new HashMap<Integer, Map<String, Object>>();
+        Map<String,Map<String, String>> categorias = new HashMap<String,Map<String, String>>();
+
+
+        while (!inscripcionCerrada) {
+            System.out.println("Crear Categorias 1." +
+                    " Registrar participante 2." +
+                    " Mostrar todos los inscriptos 3." +
+                    " Desinscribir participante 4." +
+                    " Calcular monto total 5." +
+                    " Salir 0.");
+            int opciones = Integer.parseInt(console.nextLine());
+            switch (opciones) {
+                case 1:
+                    //region Crear Categoria
+                    System.out.print("Ingrese el id: ");
+                    String id = console.nextLine();
+                    System.out.print("Ingrese el nombre de la categoria: ");
+                    String nombreCategoria = console.nextLine();
+                    System.out.print("Ingrese la descripción de la categoria: ");
+                    String descripcionCategoria = console.nextLine();
+
+                    List<String> listaCategoria = new ArrayList<String>();
+                    listaCategoria.add(id);
+                    listaCategoria.add(nombreCategoria);
+                    listaCategoria.add(descripcionCategoria);
+
+                    listaCategorias.add(listaCategoria);
+                    System.out.println("categoria agregada correctamente");
+                    //endregion
+                    break;
+                case 2:
+                    //region Registrar participante
+
+                    System.out.print("Ingrese el número de participante: ");
+                    int numeroParticipante = Integer.parseInt(console.nextLine());
+
+                    System.out.print("Ingrese DNI: ");
+                    String dni = console.nextLine();
+
+                    System.out.print("Ingrese nombre: ");
+                    String nombre = console.nextLine();
+
+                    System.out.print("Ingrese apellido: ");
+                    String apellido = console.nextLine();
+
+                    System.out.print("Ingrese edad: ");
+                    int edad = Integer.parseInt(console.nextLine());
+
+                    System.out.print("Ingrese número de celular: ");
+                    int celular = Integer.parseInt(console.nextLine());
+
+                    System.out.print("Ingrese número de emergencia: ");
+                    int numeroEmergencia = Integer.parseInt(console.nextLine());
+
+                    System.out.print("Ingrese grupo sanguíneo: ");
+                    String grupoSanguineo = console.nextLine();
+
+                    System.out.print("Ingrese categoria chico 1. medio 2. avanzado 3.");
+                    int categoria = Integer.parseInt(console.nextLine());
+
+                    String montoAAbonar = "";
+                    switch (categoria){
+                        case 1:
+                            montoAAbonar = edad <= 18 ? "$1300" : "$1500";
+                            break;
+                        case 2:
+                            montoAAbonar = edad <= 18 ? "$2000" : "$2300";
+                            break;
+                        case 3:
+                            montoAAbonar = "$2800";
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                    // Crear un Map para almacenar los datos del participante
+                    Map<String, Object> datosParticipante = new HashMap<String, Object>();
+                    datosParticipante.put("DNI", dni);
+                    datosParticipante.put("Nombre", nombre);
+                    datosParticipante.put("Apellido", apellido);
+                    datosParticipante.put("Edad", edad);
+                    datosParticipante.put("Celular", celular);
+                    datosParticipante.put("NumeroEmergencia", numeroEmergencia);
+                    datosParticipante.put("GrupoSanguineo", grupoSanguineo);
+                    datosParticipante.put("Categoria", categoria);
+                    datosParticipante.put("MontoAPagar", montoAAbonar);
+
+
+                    // Almacenar el Map de datos en el Map de participantes
+                    participantes.put(numeroParticipante, datosParticipante);
+                    break;
+
+                    //endregion
+                case 3:
+                    //TODO
+                    //region Mostrar todos los inscriptos
+
+                    //endregion
+                    break;
+                case 4:
+                    //TODO
+                    //region Desinscribir participante
+
+                    //endregion
+                    break;
+                case 5:
+                    //TODO
+                    //region Calcular monto total
+
+                    //endregion
+                    break;
+                default:
+                    //TODO
+                    //region Salir
+
+                    System.out.println("Seguro que quieres salir? true/false:");
+                    inscripcionCerrada = Boolean.parseBoolean(console.nextLine());
+
+                    //endregion
+            }
+
+        }
+
+        //endregion
+
+
+    }
+}
