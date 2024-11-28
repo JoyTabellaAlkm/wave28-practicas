2704bc9d931f58385d161b8653283ecad7b00022
Daniel Franco <danielfrancocabrera3@gmail.com>
2024-11-28 11:38:33 -0500

    Primer ejercicio de java resuelto - ciudades y climas

---
+++ 2. java/ejercicioCiudadesClimas/solucion.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String [] args) {
+        String ciudades[] = {"Londres",
+                "Madrid",
+                "Nueva York",
+                "Buenos Aires",
+                "Asunción",
+                "São Paulo",
+                "Lima",
+                "Santiago de Chile",
+                "Lisboa",
+                "Tokio"};
+        int temperaturas[][] ={{-2,33},{-3,32},{-8,27},{4,37},{6,42},{5,43},{0,39},{-7,26},{-1,31},{-10,35}};
+        String paisMayorTemperatura = ciudades[0];
+        String paisMenorTemperatura = ciudades[0];
+        int mayorTemperatura = temperaturas[0][0];
+        int menorTemperatura = temperaturas[0][0];
+        for (int i = 0; i< temperaturas.length; i++){
+            for (int j= 0; j < 2; j++){
+                if (temperaturas[i][j]> mayorTemperatura){
+                    mayorTemperatura = temperaturas[i][j];
+                    paisMayorTemperatura = ciudades[i];
+                }
+                if (temperaturas[i][j] < menorTemperatura){
+                    menorTemperatura = temperaturas[i][j];
+                    paisMenorTemperatura = ciudades[i];
+                }
+            }
+        }
+
+        //for (int i = 0; i< temperaturas.length; i++){
+        //    if (temperaturas[i][1]> mayorTemperatura){
+        //        mayorTemperatura = temperaturas[i][1];
+        //        paisMayorTemperatura = ciudades[i];
+        //    }
+        //    if (temperaturas[i][0] < menorTemperatura){
+        //        menorTemperatura = temperaturas[i][0];
+        //        paisMenorTemperatura = ciudades[i];
+        //    }
+
+        //}
+        System.out.println("La MAYOR temp. la tuvo: " + paisMayorTemperatura + ", con " + mayorTemperatura+"°");
+        System.out.println("La MENOR temp. la tuvo: " + paisMenorTemperatura + ", con " + menorTemperatura+"°");
+    }
+}
