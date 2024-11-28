f31684a60122885abf184b666db008a7d18eb9be
santiago.parrado <santi.542001@gmail.com>
2024-11-28 11:01:38 -0500

    Ejercicio temperaturas

---
+++ 2. java/Main.java
@@ -0,0 +1,46 @@
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+        
+            String[] ciudades = {"Londres", "Madrid", "Nueva York", "Buenos Aires","Asuncion", "São Paulo", "Lima", "Santiago de Chile", "Lisboa", "Tokyo"};
+            int[][] temperaturas = 
+                    {
+                        {-2, 33},
+                        {-3, 32},
+                        {-8, 27},
+                        {4, 37},
+                        {6, 42},
+                        {5, 43},
+                        {0, 39},
+                        {-7, 26},
+                        {-1, 31},
+                        {-10, 35}
+                    };
+            int minTemp = temperaturas[0][0];
+            int maxTemp = temperaturas[0][1];
+            String ciudadMinTemp = ciudades[0];
+            String ciudadMaxTemp = ciudades[0];
+
+            for (int i=0 ; i<temperaturas.length ; i++) 
+            {
+
+               int tempMinActual = temperaturas[i][0];
+               int tempMaxActual = temperaturas[i][1];
+
+               if (tempMinActual < minTemp)
+               {
+                   minTemp = tempMinActual;
+                   ciudadMinTemp = ciudades[i];
+               }
+
+               if (tempMaxActual > maxTemp)
+               {
+                   maxTemp = tempMaxActual;
+                   ciudadMaxTemp = ciudades[i];
+               }
+            }
+                System.out.println("La ciudad con mayor temperatura es " + ciudadMaxTemp + " y fue de " + maxTemp);
+                System.out.println("La ciudad con menor temperatura es " + ciudadMinTemp + " y fue de " + minTemp);
+    }
+}
