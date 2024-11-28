5011cff5ab5db9b601a1b92e8f6d22d157acea2e
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2024-11-28 18:29:49 -0500

    Optimización de código en la búsqueda de temps

--- 2. java/practica_temperaturas_globales/src/meli/practica/Main.java
+++ 2. java/practica_temperaturas_globales/src/meli/practica/Main.java
@@ -1,4 +1,5 @@
 package practica_temperaturas_globales.src.meli.practica;
+
 import java.util.Scanner;
 
 public class Main {
@@ -18,50 +19,37 @@ public class Main {
         System.out.println("Por favor carga los datos de temperatura de las ciudades de la tabla");
         System.out.println("---------------------------------------------------------");
 
-        for (int indiceCiudades = 0; indiceCiudades <= ciudades.length; indiceCiudades++) {
+        for (int indiceCiudades = 0; indiceCiudades < ciudades.length; indiceCiudades++) {
+            System.out.print("Ingrese la temperatura mínima para la ciudad de " + ciudades[indiceCiudades] + ": ");
+            matrizTemperaturas[indiceCiudades][0] = teclado.nextDouble();
 
-            for (int i = 0; i <= 1; i++) {
-                if (i == 0) {
-                    System.out.print("Ingrese la temperatura mínima para la ciudad de " + ciudades[indiceCiudades] + ": ");
-                    matrizTemperaturas[indiceCiudades][i] = teclado.nextInt();
-                } else {
-                    System.out.print("Ingrese la temperatura máxima para la ciudad de: " + ciudades[indiceCiudades] + ": ");
-                    matrizTemperaturas[indiceCiudades][i] = teclado.nextInt();
-                }
-            }
+            System.out.print("Ingrese la temperatura máxima para la ciudad de " + ciudades[indiceCiudades] + ": ");
+            matrizTemperaturas[indiceCiudades][1] = teclado.nextDouble();
 
             System.out.println("---------------------------------------------------------");
-
         }
 
         teclado.close();
 
         tempMinima = matrizTemperaturas[0][0];
         nombreCiudadMinima = ciudades[0];
-        tempMaxima = matrizTemperaturas[0][0];
+        tempMaxima = matrizTemperaturas[0][1];
         nombreCiudadMaxima = ciudades[0];
 
-        for (int indiceCiudades = 0; indiceCiudades <= ciudades.length; indiceCiudades++) {
-            for (int i = 0; i <= 1; i++) {
-
-                if (matrizTemperaturas[indiceCiudades][i]<tempMinima){
-                    tempMinima=matrizTemperaturas[indiceCiudades][i];
-                    nombreCiudadMinima=ciudades[indiceCiudades];
-                }
-
-                if (matrizTemperaturas[indiceCiudades][i]>tempMaxima){
-                    tempMaxima=matrizTemperaturas[indiceCiudades][i];
-                    nombreCiudadMaxima=ciudades[indiceCiudades];
-                }
-
+        for (int indiceCiudades = 0; indiceCiudades < ciudades.length; indiceCiudades++) {
+            if (matrizTemperaturas[indiceCiudades][0] < tempMinima) {
+                tempMinima = matrizTemperaturas[indiceCiudades][0];
+                nombreCiudadMinima = ciudades[indiceCiudades];
             }
-
+            if (matrizTemperaturas[indiceCiudades][1] > tempMaxima) {
+                tempMaxima = matrizTemperaturas[indiceCiudades][1];
+                nombreCiudadMaxima = ciudades[indiceCiudades];
+            }
         }
 
         System.out.println("Resultados");
         System.out.println("La temperatura mínima es de: " + tempMinima + " grados celsius en la ciudad de: " + nombreCiudadMinima);
         System.out.println("---------");
         System.out.println("La temperatura máxima es de: " + tempMaxima + " grados celsius en la ciudad de: " + nombreCiudadMaxima);
-
     }
 }
