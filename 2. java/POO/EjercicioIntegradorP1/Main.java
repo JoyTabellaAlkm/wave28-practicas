d8f8502725050bf517dfbcf704274f2f52b2af1e
lucasbianchi0 <lucmbianchi2000@gmail.com>
2024-11-29 11:59:25 -0300

    Se sube ejercicio grupal p1 poo

---
+++ 2. java/POO/EjercicioIntegradorP1/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import com.company.model.Persona;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Persona p1 = new Persona("Guille", 25, "33333333", 75.5, 1.80);
+        Persona p2 = new Persona("Fer", 25, "33343333");
+//        Persona p3 = new Persona("Fer", 25 ); No es posible porque no hay un constructor definido con estos parametros
+        Persona p3 = new Persona();
+
+        System.out.println(p1);
+
+        int imc = p1.calcularImc();
+        switch (imc){
+            case -1:
+                System.out.println("Estas bajo peso");
+                break;
+            case 0:
+                System.out.println("Estas en un peso saludable");
+                break;
+            case 1:
+                System.out.println("Estas en sobre peso");
+                break;
+        }
+
+
+        if(p1.esMayorDeEdad()){
+            System.out.println("Es mayor de edad");
+        }else{
+            System.out.println("No es mayor de edad");
+        }
+
+
+
+
+
+    }
+}
---
+++ 2. java/POO/EjercicioIntegradorP1/Persona.java
@@ -0,0 +1,56 @@
+package com.company.model;
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, int edad, String dni) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+
+
+    public int calcularImc(){
+        double imc = peso / (altura*altura) ;
+        if(imc < 20){
+            return -1;
+        }else if(imc >= 20 && imc <= 25){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad(){
+        return edad >=18;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", edad=" + edad +
+                ", dni='" + dni + '\'' +
+                ", peso=" + peso +
+                ", altura=" + altura +
+                '}';
+    }
+
+
+}
