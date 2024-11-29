df4387a5c424e9563945c81db1140962cb8736c8
elPeiretti <tomas.peiretti@mercadolibre.com>
2024-11-29 17:49:52 -0300

    agrega resolucion parcial ejercicio 4 java

---
+++ 2. java/ejercicios_poo1/java-ej4/src/NoPerecedero.java
@@ -0,0 +1,29 @@
+public class NoPerecedero extends Producto {
+
+    private String tipo;
+
+    public NoPerecedero(String nombre, double precio, String tipo) {
+        super(nombre, precio);
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public String toString() {
+        return "NoPerecedero{" +
+                "tipo='" + tipo + '\'' +
+                '}';
+    }
+
+    @Override
+    public double calcular(int cantProductos) {
+        return super.calcular(cantProductos);
+    }
+}
