7c4a8e540ad0680ca12c4c73a049891171ae480f
klaus <nicolasparias26@gmail.com>
2024-11-29 12:54:51 -0500

    feat: adding runners

--- 2. java/tarea_2/Main.java
+++ 2. java/tarea_2/Main.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.function.*;
 
 public class Main {
   
@@ -23,13 +24,15 @@ public class Main {
   static Map<Integer, String>[] lst_name = new HashMap[3];
   static Map<Integer, String>[] cel = new HashMap[3];
   static Map<Integer, String>[] rh = new HashMap[3];
+  static Map<Integer, Integer>[] age = new HashMap[3];
+  static Map<Integer, String>[] em_cel = new HashMap[3];//emergency
 
   // methods 
   static void show_categories(){
     System.out.println("CATEGORIES FOR OUR MARATHONE");
       System.out.println("==================================================================");
     for(int i = 0; i < 3; ++i){
-      System.out.println(i + "name: " + name_category[i]);
+      System.out.println(i + "Name: " + name_category[i]);
       System.out.println("Description: ");
       System.out.println("Distance: " + distance[i]);
       System.out.println("Forest: " + forest[i]);
@@ -40,8 +43,38 @@ public class Main {
     }
   }
 
+
   public static void main(String[] args) {
     show_categories();
+    
+    //Runners to add ==========================
+    int[] codes_add = {26, 16, 3, 21};
+    String[] names_add = {"Nicolas", "Mariana", "Oscar", "Luisa"};
+    String[] dni_add = {"0001", "0010", "0011", "0100"};
+    String[] lst_name_add = {"Arias", "Ramirez", "Sierra", "Arias"};
+    String[] cel_add = {"42", "42", "42", "42"};
+    String[] rh_add = {"A-", "O+", "B+", "AB"};
+    String[] em_cel_add = {"42", "42", "42"};
+    int[] age_add = {25, 19, 22, 24};
+    //=========================================
 
+    //Runners to delete =======================
+    int code_del = 26; //Nicolas will be deleted
+    //=========================================
+
+    BiConsumer<Integer, Integer> add_runner = (Integer i, Integer cat) -> {// cat for category
+      code[cat].add(codes_add[cat]);
+      names[cat].put(codes_add[cat], names_add[cat]);
+      lst_name[cat].put(codes_add[cat], lst_name_add[cat]);
+      dni[cat].put(codes_add[cat], names_add[cat]);
+      cel[cat].put(codes_add[cat], names_add[cat]);
+      rh[cat].put(codes_add[cat], rh_add[cat]);
+      age[cat].put(codes_add[cat], age_add[cat]);
+    };
+
+    int n = 4;
+    for(int i = 0; i < n; ++i){
+      add_runner.accept(i,(i % 3));
+    }
   }
 }
