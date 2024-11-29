d3c52897f0ab3d628dcb68b679e2f26193f1c7b5
JoaquinDamianSeita <joaquindamianseita@gmail.com>
2024-11-29 18:02:56 -0300

    add POO ejercio 1 y 2

---
+++ 2. java/POO2/src/PracticasExcepciones.java
@@ -0,0 +1,13 @@
+public class PracticasExcepciones {
+    int a;
+    int b;
+
+    public PracticasExcepciones(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int calcularCociente() {
+        return a / b;
+    }
+}
---
+++ 2. java/POO2Prod/src/Perecedero.java
@@ -0,0 +1,27 @@
+public class Perecedero extends Producto {
+    int diasPorCaducar;
+
+    public Perecedero(String nombre, double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public double calcularPrecio(int cantidadDeProductos) {
+        double total = super.calcularPrecio(cantidadDeProductos);
+        return switch (diasPorCaducar) {
+            case 1 -> total / 4;
+            case 2 -> total / 3;
+            case 3 -> total / 2;
+            default -> total;
+        };
+    }
+}
