b2b8fb8968d747839fb774fa08bd97d50f6ab39a
fdelavalle <delavallef4@gmail.com>
2024-11-29 18:09:18 -0300

    Added ejercicio excepciones

---
+++ 2. java/ejercicio-excepciones/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        PracticaExcepciones practica = new PracticaExcepciones();
+        try {
+            practica.calcularCociente();
+        } catch (Exception e) {
+            System.out.println("Se ha producido un error.");
+        } finally {
+            System.out.println("Programa finaliz" +
+                    "ado.");
+        }
+
+        try{
+            practica.calcularCociente();
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException("No se puede dividir por cero.");
+        }
+
+    }
+}
---
+++ 2. java/ejercicio-excepciones/PracticaExcepciones.java
@@ -0,0 +1,8 @@
+public class PracticaExcepciones {
+    int a = 0;
+    int b = 0;
+
+    public double calcularCociente(){
+        return b/a;
+    }
+}
