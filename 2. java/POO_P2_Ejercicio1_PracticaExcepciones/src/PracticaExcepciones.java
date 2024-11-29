c451bf0b2ff7e913d5071a4e5b8a10941e5f5223
astrid21 <astridmalamud@gmail.com>
2024-11-29 17:38:41 -0300

    POO P2 Ejercicios 1 y 2

---
+++ 2. java/POO_P2_Ejercicio1_PracticaExcepciones/src/PracticaExcepciones.java
@@ -0,0 +1,14 @@
+public class PracticaExcepciones {
+    int a;
+    int b;
+
+    public PracticaExcepciones() {
+        this.a = 0;
+        this.b = 300;
+    }
+
+    public int cociente(){
+        return b/a;
+    }
+
+}
