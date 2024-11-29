44e0a6be80587e0caa25ae57e4ac8cbff902f18f
albaqueroh <andres.baquero6@gmail.com>
2024-11-29 15:24:44 -0500

    ejericio de excepciones

---
+++ 2. java/excepciones/src/main/java/com/mercadolibre/Main.java
@@ -0,0 +1,9 @@
+package com.mercadolibre;
+
+public class Main {
+    public static void main(String[] args) {
+        PracticaExcepciones pe = new PracticaExcepciones();
+        pe.calcularCociente1();
+        pe.calcularCociente2();
+    }
+}
