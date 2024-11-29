f8c7181050d422f4971630033270b5a78e1b07ed
klaus <nicolasparias26@gmail.com>
2024-11-29 13:19:55 -0500

    feat: tarea 2 almost finished

--- 2. java/tarea_2/Main.java
+++ 2. java/tarea_2/Main.java
@@ -10,6 +10,7 @@ public class Main {
   
   // id, name, description
   static int[] id_category = {0, 1, 2};
+  static int[] pay = {0, 0, 0};
   static String[] name_category = {"Beginner", "Regular", "Grand"};
   static int[] distance = {2, 5, 10};
   static boolean[] forest = {true, true, true};
@@ -26,6 +27,7 @@ public class Main {
   static Map<Integer, String>[] rh = new HashMap[3];
   static Map<Integer, Integer>[] age = new HashMap[3];
   static Map<Integer, String>[] em_cel = new HashMap[3];//emergency
+  static Map<Integer, Integer> category_by_runner = new HashMap<>();
 
   // methods 
   static void show_categories(){
@@ -43,6 +45,8 @@ public class Main {
     }
   }
 
+  static void show_by_category(){
+  }
 
   public static void main(String[] args) {
     show_categories();
@@ -55,7 +59,7 @@ public class Main {
     String[] cel_add = {"42", "42", "42", "42"};
     String[] rh_add = {"A-", "O+", "B+", "AB"};
     String[] em_cel_add = {"42", "42", "42"};
-    int[] age_add = {25, 19, 22, 24};
+    int[] age_add = {17, 18, 22, 24};
     //=========================================
 
     //Runners to delete =======================
@@ -70,11 +74,52 @@ public class Main {
       cel[cat].put(codes_add[cat], names_add[cat]);
       rh[cat].put(codes_add[cat], rh_add[cat]);
       age[cat].put(codes_add[cat], age_add[cat]);
+      em_cel[cat].put(codes_add[cat], em_cel_add[cat]);
     };
+
+    BiFunction<Integer, Integer, Integer> value_pay = (Integer cat, Integer age_cur) -> {
+      if(cat == 0){
+        return age_cur >= 18 ? 1500 : 1300;
+      } else if( cat == 1 ){
+        return age_cur >= 18 ? 2300 : 2000;
+      }else return 2800;
+    };
+
+    //ADDING RUNNERS
 
     int n = 4;
     for(int i = 0; i < n; ++i){
-      add_runner.accept(i,(i % 3));
+      add_runner.accept(i, i % 3);
+      pay[i % 3] += value_pay.apply(i % 3, age_add[i]);
+      category_by_runner.put(codes_add[i], i % 3);
     }
+
+    //SHOW RUNNERS BY CATEGORY
+    show_by_category();
+
+    //DELETE Runner
+    int id = code_del;
+    int is_category = category_by_runner.get(id);
+    pay[is_category] -= value_pay.apply(is_category, age[is_category].get(id));
+    code[is_category].remove(id);
+    names[is_category].remove(id);
+    lst_name[is_category].remove(id);
+    cel[is_category].remove(id);
+    dni[is_category].remove(id);
+    rh[is_category].remove(id);
+    age[is_category].remove(id);
+    em_cel[is_category].remove(id);
+
+    show_by_category();
+
+    //SHOW AMOUNT PAID
+    int total = 0;
+    for(int i = 0; i < 3; ++i){
+      System.out.println("Category id: " + i);
+      System.out.println("Amount paid: $" + pay[i]);
+      total += pay[i];
+    }
+    
+    System.out.println("Total amount: $" + total);
   }
 }
