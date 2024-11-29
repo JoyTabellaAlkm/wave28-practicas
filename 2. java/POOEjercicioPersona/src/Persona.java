996a67c9257ecf2e1c74d9362aca97f10b6f6de8
martinsimonetti <msimonetti@gmail.com>
2024-11-29 11:52:07 -0300

    Realizamos en equipo el ejercicio persona.

---
+++ 2. java/POOEjercicioPersona/src/Persona.java
@@ -0,0 +1,51 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String dni;
+    private double peso;
+    private double altura;
+
+    public Persona(){
+
+    }
+
+    public Persona(String nombre, int edad, String dni){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    public Persona(String nombre, int edad, String dni, double peso, double altura) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni;
+        this.peso = peso;
+        this.altura = altura;
+    }
+
+    public int calcularMasaCorporal(){
+        double masaCorporal = this.peso/(this.altura*this.altura);
+
+        if(masaCorporal<20){
+            return -1;
+        } else if(masaCorporal<=25){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad (){
+        return this.edad >= 18;
+    }
+
+    public String toString() {
+        return "Persona{" + "\n" +
+                "nombre: '" + nombre + '\'' + ",\n" +
+                "edad: " + edad + ",\n" +
+                "dni: '" + dni + '\'' + ",\n" +
+                "peso: " + peso + ",\n" +
+                "altura: " + altura + ",\n" +
+                '}';
+    }
+}
