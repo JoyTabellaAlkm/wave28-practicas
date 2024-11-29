2ac1b82ab60372a62d293773a28cfa0ce3ca0e83
SantiMar00 <santimarinio2000@gmail.com>
2024-11-29 12:05:19 -0300

    add ejercicio 1 POO

---
+++ 2. java/poo-ejercicio-1/Main.java
@@ -0,0 +1,14 @@
+package poo.p1.ejercicios_sincronicos;
+
+public class Main {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona();
+        Persona persona2 = new Persona("Leandro Brey", 21, "44235945");
+        Persona persona3 = new Persona("Camila Giudisse", 28, "40395234", 65.5, 1.63);
+
+        persona3.imprimirResultadoIMC();
+        System.out.println("¿Es mayor de edad? " + persona3.esMayorDeEdad());
+        System.out.println("Datos de la persona:");
+        System.out.println(persona3);
+    }
+}
