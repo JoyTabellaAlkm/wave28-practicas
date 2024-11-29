d23558ac79cb39b6946ce7d5adfa225ab5ca4624
guille-kalniker <guillermo.kalniker@mercadolibre.com>
2024-11-29 14:59:58 -0300

    Subo ejercicio 2 de LH del Automovil

---
+++ 2. java/POO/Ej LH 2/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Automovil automovil = new Automovil("Honda", "bordo", 127722.82);
+
+        System.out.println(automovil.mostrarMarcaYColor());
+
+
+    }
+}
