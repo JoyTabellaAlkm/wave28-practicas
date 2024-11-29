45357a89c19bd8a2a78f8973a488e07b624c22dc
“rivejjj” <rivero.joaquinn@gmail.com>
2024-11-29 12:12:27 -0300

    avances de ejercicio carrera incompleto

---
+++ 2. java/carrera/src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        List<HashMap> circuito_chico = new ArrayList<HashMap>();
+        HashMap<String,String> inscripcion = new HashMap<>();
+        HashMap<String,String> participante = new HashMap<>();
+    }
+}
