6b631eafed0915d36eada2d391a6999dbee1f0ac
Karina Sierra Velandia <kkarinasierrav@gmail.com>
2024-11-29 10:47:35 -0500

    ejercicio del automovil poo

--- 2. java/poo/practica2_poo_diseño_clases_metodos/src/meli/practica/Automovil.java
+++ 2. java/poo/practica2_poo_diseño_clases_metodos/src/meli/practica/Automovil.java
@@ -1,3 +1,23 @@
 public class Automovil {
-    
+
+    private String marca;
+    private String color;
+    private double kilometros;
+
+    public Automovil(){
+        this.marca = "";
+        this.color = "";
+        this.kilometros = 0.0;
+    }
+
+    public Automovil(String marca, String color, double kilometros){
+        this.marca = marca;
+        this.color = color;
+        this.kilometros = kilometros;
+    }
+
+    public String mostrarMarcaYColor(){
+        String marcaYColor = "la marca del auto es: " + this.marca + ". El color del auto es: " +  this.color;
+        return marcaYColor;
+    }
 }
