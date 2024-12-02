fe1fe4af436cd9d928b0116e1ba9c16af6cb9e9b
nadinajauch <nadinajauch@gmail.com>
2024-12-02 15:53:17 -0300

    agrego resolucion que faltaba de ej intro java p2, vivo

--- 2. java/Intro_P2_Vivo/Main.java
+++ 2. java/Intro_P2_Vivo/Main.java
@@ -2,21 +2,42 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, Map<String, String>> categorias = Map.of(
-                1, Map.of("nombre", "Circuito chico",
-                        "descripcion", "2 km por selva y arroyos"),
-                2, Map.of("nombre", "Circuito medio",
-                        "descripcion", "5 km por selva, arroyos y barro"),
-                3, Map.of("nombre", "Circuito avanzado",
-                        "descripcion", "10 km por selva, arroyos, barro y escalada en piedra")
-        );
+        Map<Integer, Map<String, String>> categorias = inicializarCategorias();
+        Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria = inicializarInscripciones();
+        agregarIncripciones(inscripcionesPorCategoria, categorias);
 
-        Map<Integer, Map<String, Object>> inscripciones = new HashMap<>();
-        Map<String, List<Map<String, Object>>> inscripcionesPorCategoria = new HashMap<>();
+        // Mostrar inscripciones agrupadas por categorias
+        mostrarInscripcionesPorCategoria(inscripcionesPorCategoria);
+
+        // Eliminar participantes
+        Map<Integer, List<Map<String, Object>>> inscripcionesActualizadas = eliminarInscriptosYMostrar(inscripcionesPorCategoria);
+
+        // Mostrar totales por categoría
+        mostrarTotalesPorCategoria(inscripcionesActualizadas);
+    }
+
+    public static Map<Integer, Map<String, String>> inicializarCategorias() {
+        return Map.of(
+                1, Map.of("nombre", "Circuito chico", "descripcion", "2 km por selva y arroyos"),
+                2, Map.of("nombre", "Circuito medio", "descripcion", "5 km por selva, arroyos y barro"),
+                3, Map.of("nombre", "Circuito avanzado", "descripcion", "10 km por selva, arroyos, barro y escalada en piedra")
+        );
+    }
+
+    public static Map<Integer, List<Map<String, Object>>> inicializarInscripciones() {
+        Map<Integer, List<Map<String, Object>>> mapInscripciones = new HashMap<>();
+        mapInscripciones.put(1, new ArrayList<>());
+        mapInscripciones.put(2, new ArrayList<>());
+        mapInscripciones.put(3, new ArrayList<>());
+        return mapInscripciones;
+    }
+
+    public static void agregarIncripciones(Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria, Map<Integer, Map<String, String>> categorias) {
         Scanner scanner = new Scanner(System.in);
-        String continuar = "s";
+        String continuarAgregandoInscriptos = "s";
         int numeroInscripcion = 1;
-        while (continuar.equals("s")) {
+
+        while (continuarAgregandoInscriptos.equals("s")) {
             Map<String, Object> participante = new HashMap<>();
 
             System.out.print("Ingrese el número de participante: ");
@@ -30,35 +51,26 @@ public class Main {
             participante.put("edad", scanner.nextInt());
             scanner.nextLine();
 
-            //hago la inscripcion
-            System.out.print("Id de categoria de circuito: ");
+            System.out.print("Id de categoría de circuito: ");
             int categoriaId = scanner.nextInt();
             scanner.nextLine();
 
-            double montoAbonar = calcularMonto(categoriaId, (int)participante.get("edad"));
+            double montoAbonar = calcularMonto(categoriaId, (int) participante.get("edad"));
             if (montoAbonar == -1) {
                 System.out.println("Inscripción no permitida para este circuito y edad.");
                 continue;
             }
 
-            Map<String,Object> inscripcion = Map.of(
+            inscripcionesPorCategoria.get(categoriaId).add(Map.of(
+                    "numeroInscripcion", numeroInscripcion,
                     "categoria", categorias.get(categoriaId),
                     "participante", participante,
-                    "monto", montoAbonar);
-            inscripciones.put(numeroInscripcion, inscripcion);
-
-            String categoriaNombre = categorias.get(categoriaId).get("nombre");
-            if (!inscripcionesPorCategoria.containsKey(categoriaNombre))
-                inscripcionesPorCategoria.put(categoriaNombre, new ArrayList<>());
-            inscripcionesPorCategoria.get(categoriaNombre).add(inscripcion);
-
-            System.out.print("¿Desea realizar otra inscripcion? (s/n): ");
-            continuar = scanner.nextLine();
+                    "monto", montoAbonar
+            ));
+            System.out.print("¿Desea realizar otra inscripción? (s/n): ");
+            continuarAgregandoInscriptos = scanner.nextLine();
             numeroInscripcion++;
         }
-
-        // muestreo
-        mostrarInscripcionesPorCategoria(inscripcionesPorCategoria);
     }
 
     public static double calcularMonto(int categoriaId, int edad) {
@@ -74,19 +86,76 @@ public class Main {
         }
     }
 
-    public static void mostrarInscripcionesPorCategoria(Map<String, List<Map<String, Object>>> mapInscripcionesPorCategoria){
-        for (Map.Entry<String, List<Map<String, Object>>> entrada : mapInscripcionesPorCategoria.entrySet()) {
-            List<Map<String, Object>> listaInscripciones = entrada.getValue();
+    public static Map<Integer, List<Map<String, Object>>> eliminarInscriptosYMostrar(Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("¿Desea quitar participantes? (s/n):");
+            if (!scanner.nextLine().equals("s"))
+                break;
 
-            System.out.println("CATEGORIA: " + entrada.getKey());
+            System.out.println("Ingrese número de participante a desinscribir:");
+            int nroParticipanteADesinscribir = scanner.nextInt();
+            scanner.nextLine();
+
+            eliminarParticipanteDeCategorias(inscripcionesPorCategoria, nroParticipanteADesinscribir);
+
+            System.out.println("Participante eliminado correctamente.");
+        }
+
+        mostrarInscripcionesPorCategoria(inscripcionesPorCategoria);
+        return inscripcionesPorCategoria;
+    }
+
+    public static void eliminarParticipanteDeCategorias(Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria,
+                                                        int nroParticipanteADesinscribir) {
+        for (List<Map<String, Object>> listaInscripciones : inscripcionesPorCategoria.values()) {
+            for (int i = 0; i < listaInscripciones.size(); i++) {
+                Map<String, Object> inscripcion = listaInscripciones.get(i);
+                Map<String, Object> participante = (Map<String, Object>) inscripcion.get("participante");
+
+                if (participante.get("numeroParticipante").equals(nroParticipanteADesinscribir)) {
+                    listaInscripciones.remove(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void mostrarInscripcionesPorCategoria(Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria) {
+        for (Map.Entry<Integer, List<Map<String, Object>>> entry : inscripcionesPorCategoria.entrySet()) {
+            List<Map<String, Object>> listaInscripciones = entry.getValue();
+            Map<String, String> categoria = inicializarCategorias().get(entry.getKey());
+
+            System.out.println("-------------------------------------");
+            System.out.println("CATEGORÍA: " + categoria.get("nombre"));
+            System.out.println("-------------------------------------");
+
             for (Map<String, Object> inscripcion : listaInscripciones) {
                 Map<String, Object> participante = (Map<String, Object>) inscripcion.get("participante");
                 System.out.println("Participante: " + participante.get("nombre"));
                 System.out.println("Número de participante: " + participante.get("numeroParticipante"));
                 System.out.println("Edad: " + participante.get("edad"));
                 System.out.println("Monto: " + inscripcion.get("monto"));
-                System.out.println("-------------------------------------");
+                System.out.println("---");
             }
+        }
+    }
+
+    public static void mostrarTotalesPorCategoria(Map<Integer, List<Map<String, Object>>> inscripcionesPorCategoria) {
+        for (Map.Entry<Integer, List<Map<String, Object>>> entry : inscripcionesPorCategoria.entrySet()) {
+            List<Map<String, Object>> listaInscripciones = entry.getValue();
+            Map<String, String> categoria = inicializarCategorias().get(entry.getKey());
+
+            System.out.println("-------------------------------------");
+            System.out.println("CATEGORÍA: " + categoria.get("nombre"));
+
+            double total = 0;
+            for (Map<String, Object> inscripcion : listaInscripciones) {
+                total += (double) inscripcion.get("monto");
+            }
+
+            System.out.println("TOTAL RECAUDADO: " + total);
+            System.out.println("-------------------------------------");
         }
     }
 }
