d7cdc5966516e262fc9a3baf710863154cc4f855
InakiEtc <inaki.etchegoyen.ie@gmail.com>
2024-12-02 10:54:58 -0300

    Clase/Ejercicio de Exceptions y Herencia

---
+++ 2. java/POODistribuidora/Perecedero.java
@@ -0,0 +1,43 @@
+public class Perecedero extends Producto {
+    private int diasPorCaducar;
+
+    public int getDiasPorCaducar() {
+        return diasPorCaducar;
+    }
+
+    public void setDiasPorCaducar(int diasPorCaducar) {
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    public Perecedero(String nombre, double precio, int diasPorCaducar) {
+        super(nombre, precio);
+        this.diasPorCaducar = diasPorCaducar;
+    }
+
+    @Override
+    public String toString() {
+        return "Dias por caducar: " + diasPorCaducar;
+    }
+
+    @Override
+    public double calcular(int cantidadDeProductos) {
+        double total = 0;
+        double precioSinDescuento = super.calcular(cantidadDeProductos);
+
+        switch (this.diasPorCaducar){
+            case 1:
+                total = precioSinDescuento * 0.25;
+                break;
+            case 2:
+                total = precioSinDescuento * 0.333333;
+                break;
+            case 3:
+                total = precioSinDescuento * 0.5;
+                break;
+                default:
+                    total = precioSinDescuento;
+                    break;
+        }
+        return total;
+    }
+}
---
+++ 2. java/POOExceptions/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PracticaExcepciones practica = new PracticaExcepciones();
+        
+        System.out.println(practica.dividirAB());
+    }
+}
