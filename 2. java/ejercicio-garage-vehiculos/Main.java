c9e1451fffec840efadafbd220d10b14725d2a5e
SantiMar00 <santimarinio2000@gmail.com>
2024-12-02 18:09:39 -0300

    add ejercicio garage

---
+++ 2. java/ejercicio-garage-vehiculos/Main.java
@@ -0,0 +1,45 @@
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        Garaje garage = new Garaje(1, List.of(
+                new Vehiculo("Ford", "Fiesta", 1000),
+                new Vehiculo("Ford", "Focus", 1200),
+                new Vehiculo("Ford", "Explorer", 2500),
+                new Vehiculo("Fiat", "Uno", 500),
+                new Vehiculo("Fiat", "Cronos", 1000),
+                new Vehiculo("Fiat", "Torino", 1250),
+                new Vehiculo("Chevrolet", "Aveo", 1250),
+                new Vehiculo("Chevrolet", "Spin", 2500),
+                new Vehiculo("Toyota", "Corolla", 1200),
+                new Vehiculo("Toyota", "Fortuner", 3000),
+                new Vehiculo("Renault", "Logan", 950)
+        ));
+
+        // Creamos una lista y le asiganamos la ordenada y despues la printamos, (la lista se imprime bien por el toString)
+        // List<Vehiculo> listaOrdenaPrecio = garage.getListaVehiculos().stream().sorted((v1,v2) -> v1.getCosto() - v2.getCosto()).toList();
+        // System.out.println(listaOrdenaPrecio);
+
+        System.out.println("Orden x precio");
+        garage.getListaVehiculos().stream().sorted((v1,v2) -> v1.getCosto() - v2.getCosto())
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Orden x Marca y Precio");
+        garage.getListaVehiculos().stream().sorted(Comparator.comparing((Vehiculo::getMarca)).thenComparing(Vehiculo::getCosto))
+                .forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Autos < a 1000");
+        garage.getListaVehiculos().stream().filter(v1 -> v1.getCosto() < 1000).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Autos >= a 1000");
+        garage.getListaVehiculos().stream().filter(v1 -> v1.getCosto() >= 1000).forEach(System.out::println);
+        System.out.println();
+
+        System.out.println("Promedio de costo");
+        System.out.println(garage.getListaVehiculos().stream().mapToInt(Vehiculo::getCosto).average().orElse(0.0));
+    }
+}
