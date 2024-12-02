9aaa23f6d71b70660c042500ef970aa8b4126d82
juanbianchi <juanbianchi@icloud.com>
2024-12-02 19:34:03 -0300

    feat: Add stream exercises

---
+++ 2. java/POO/recapitulando/streams/src/Main.java
@@ -0,0 +1,53 @@
+import models.Garage;
+import models.Vehiculo;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vehiculo vehiculo1 = new Vehiculo("Ford", "Fiesta", 1000);
+        Vehiculo vehiculo2 = new Vehiculo("Ford", "Focus", 1200);
+        Vehiculo vehiculo3 = new Vehiculo("Ford", "Explorer", 2500);
+        Vehiculo vehiculo4 = new Vehiculo("Fiat", "Uno", 500);
+        Vehiculo vehiculo5 = new Vehiculo("Fiat", "Cronos", 1000);
+        Vehiculo vehiculo6 = new Vehiculo("Fiat", "Torino", 1250);
+        Vehiculo vehiculo7 = new Vehiculo("Chevrolet", "Aveo", 1250);
+        Vehiculo vehiculo8 = new Vehiculo("Chevrolet", "Spin", 2500);
+        Vehiculo vehiculo9 = new Vehiculo("Toyota", "Corola", 1200);
+        Vehiculo vehiculo10 = new Vehiculo("Toyota", "Fortuner", 3000);
+        Vehiculo vehiculo11 = new Vehiculo("Renault", "Logan", 950);
+
+        List<Vehiculo> vehiculoList = new ArrayList<>();
+        vehiculoList.add(vehiculo1);
+        vehiculoList.add(vehiculo2);
+        vehiculoList.add(vehiculo3);
+        vehiculoList.add(vehiculo4);
+        vehiculoList.add(vehiculo5);
+        vehiculoList.add(vehiculo6);
+        vehiculoList.add(vehiculo7);
+        vehiculoList.add(vehiculo8);
+        vehiculoList.add(vehiculo9);
+        vehiculoList.add(vehiculo10);
+        vehiculoList.add(vehiculo11);
+
+        Garage garage = new Garage(1, vehiculoList);
+
+        List<Vehiculo> vehiculosOrdenadosXMayor =  vehiculoList.stream().sorted((v1,v2) -> v2.getCosto() - v1.getCosto() ).toList();
+        List<Vehiculo> vehiculosOrdenadosXMenor =  vehiculoList.stream().sorted((v1,v2) -> v1.getCosto() - v2.getCosto() ).toList();
+        System.out.println("VEHICULOS ORDENADOS DE MENOR A MAYOR: ");
+        vehiculosOrdenadosXMenor.forEach(System.out::println);
+
+        List<Vehiculo> vehiculosOrd = vehiculoList.stream().sorted(Comparator.comparing(Vehiculo::getMarca).thenComparing(Vehiculo::getCosto)).toList();
+        System.out.println("VEHICULOS ORDENADOS POR MARCA Y PRECIO:");
+        vehiculosOrd.forEach(System.out::println);
+
+        List<Vehiculo> vehiculosCostoMenorA1000 = vehiculoList.stream().filter(vehiculo -> vehiculo.getCosto() < 1000).toList();
+        List<Vehiculo> vehiculosCostoMayorA1000 = vehiculoList.stream().filter(vehiculo -> vehiculo.getCosto() >= 1000).toList();
+
+        double precioPromedio = vehiculoList.stream().mapToDouble(Vehiculo::getCosto).average().orElse(0.0);;
+        System.out.println("Precio promedio de todos los vehiculos: " + String.format("%.2f", precioPromedio));
+    }
+}
