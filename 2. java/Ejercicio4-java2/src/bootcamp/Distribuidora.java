cf42fd15d0fad980115ec0322641252e39fa899f
Franmuslera <francisco.muslera@gmail.com>
2024-12-02 09:35:07 -0300

    Ejercicio sub ejercicios POO clase 2

---
+++ 2. java/Ejercicio4-java2/src/bootcamp/Distribuidora.java
@@ -0,0 +1,17 @@
+package bootcamp;
+
+import java.util.ArrayList;
+
+public class Distribuidora {
+    public static void main(String[] args) {
+        ArrayList<Producto> productos = new ArrayList<>();
+        productos.add(new Perecedero("Carne", 8000, 1));
+        productos.add(new NoPerecedero("Fideos", 800, "Pasta"));
+
+
+        for (Producto producto : productos) {
+            System.out.println("El precio total de 5 productos de: " + producto.getName() + " es " + producto.calculate(5));
+        }
+
+    }
+}
