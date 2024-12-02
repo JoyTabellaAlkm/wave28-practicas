ea330b24f05f0d89f9a11e3ab8cc76e2d248f36d
guille-kalniker <guillermo.kalniker@mercadolibre.com>
2024-12-02 10:50:46 -0300

    Subo ejercicio 1 y 2 de la clase en vivo

---
+++ 2. java/POO/Ej grupal 2/Excepciones.java
@@ -0,0 +1,17 @@
+public class Practica_Excepciones {
+    public static void main(String[] args) {
+        int a = 0, b = 300;
+        try {
+            System.out.println(b / a);
+        } catch (Exception e) {
+            // System.out.println("Se ha producido un error");
+            throw new IllegalArgumentException("No se puede dividir por 0");
+        } finally {
+            System.out.println("Programa finalizado");
+        }
+    }
+}
+
+
+
+
