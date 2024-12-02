a10ac924849e2889e70ae8ba60963b2592c64507
redflox <swsbmm@gmail.com>
2024-12-02 16:22:04 -0500

    feat: added POO-P2-VIVO-Ejercicio2

---
+++ 2. java/POO-P2-VIVO-Ejercicio2/Producto.java
@@ -0,0 +1,39 @@
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public Producto setNombre(String nombre) {
+        this.nombre = nombre;
+        return this;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public Producto setPrecio(double precio) {
+        this.precio = precio;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "nombre='" + nombre + '\'' +
+                ", precio=" + precio +
+                '}';
+    }
+
+    public double calcular(int cantidadProductos) {
+        return precio * cantidadProductos;
+    }
+}
