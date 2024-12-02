8a018afc2a9e02d69f00e31baf4ab0a8ef1d8a5c
astrid21 <astridmalamud@gmail.com>
2024-12-02 18:03:49 -0300

    Ejercicio garaje y vehiculos

---
+++ 2. java/RecapitulandoJAVA_Ejercicios/src/Garaje.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garaje {
+    private Integer id;
+    private List<Vehiculo> listaVehiculos;
+
+    public Garaje(Integer id) {
+        this.id = id;
+        this.listaVehiculos = new ArrayList<>();
+    }
+
+    public Garaje(Integer id, List<Vehiculo> listaVehiculos) {
+        this.id = id;
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getListaVehiculos() {
+        return listaVehiculos;
+    }
+
+    public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
+        this.listaVehiculos = listaVehiculos;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Garaje{" +
+                "id=" + id +
+                ", listaVehiculos=" + listaVehiculos +
+                '}';
+    }
+}
