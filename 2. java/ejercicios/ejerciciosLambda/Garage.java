8ca61b541f5f5426112d2d02e0d1ae41c3b051ec
Caroschwab97 <caroschwab97@gmail.com>
2024-12-02 17:49:58 -0300

    Subida ejercicios Lambda, streams

---
+++ 2. java/ejercicios/ejerciciosLambda/Garage.java
@@ -0,0 +1,32 @@
+package ejercicios.ejerciciosLambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Garage {
+    private int id;
+    private List<Vehiculo> vehiculos;
+
+    public Garage(int id) {
+        this.id = id;
+        this.vehiculos = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+
+}
