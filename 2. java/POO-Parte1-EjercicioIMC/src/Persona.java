ec0af7c6c34cd7e8ccfc688e59370f7cce1c6442
Daniel Franco <danielfrancocabrera3@gmail.com>
2024-12-01 22:54:03 -0500

    Se agrega la clase persona para completar el ejercicio

--- 2. java/POO-Parte1-EjercicioIMC/src/Persona.java
+++ 2. java/POO-Parte1-EjercicioIMC/src/Persona.java
@@ -1,2 +1,51 @@
-package PACKAGE_NAME;public class Persona {
+public class Persona {
+    String nombre;
+    String dni;
+    int edad;
+    double altura;
+    double peso;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String dni, int edad) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+    }
+
+    public Persona(String nombre, String dni, int edad, double altura, double peso) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public int calcularIMC() {
+        double imc = this.peso / (this.altura * this.altura);
+        if (imc < 20) {
+            return -1;
+        }
+        else if (imc >= 20 && imc <= 25) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public boolean esMayorDeEdad() {
+        return this.edad >= 18;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", dni='" + dni + '\'' +
+                ", edad=" + edad +
+                ", altura=" + altura +
+                ", peso=" + peso +
+                '}';
+    }
 }
