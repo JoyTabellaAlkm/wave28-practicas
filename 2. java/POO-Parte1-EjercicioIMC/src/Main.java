b59fe777c4a6bef9414101781d5acc5763f5f635
Daniel Franco <danielfrancocabrera3@gmail.com>
2024-12-01 22:49:00 -0500

    POO parte 1 ejercicio de calculo de IMC

---
+++ 2. java/POO-Parte1-EjercicioIMC/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+    public static void main(String[] args) {
+        Persona personaSinAtributos = new Persona();
+        Persona personaConNombreDniEdad = new Persona("Juan", "1234", 10);
+        Persona personaConTodosLosAtributos = new Persona("Jose", "1222", 20, 1.80, 80.0);
+
+        int imc = personaConTodosLosAtributos.calcularIMC();
+        boolean esMayor = personaConTodosLosAtributos.esMayorDeEdad();
+
+        switch (imc) {
+            case -1:
+                System.out.println("Bajo peso");
+                break;
+            case 0:
+                System.out.println("Peso saludalbe");
+                break;
+            case 1:
+                System.out.println("Sobrepeso");
+                break;
+            default:
+                System.out.println("El imc esta mal!");
+                break;
+        }
+
+        if (esMayor) {
+            System.out.println("Es mayor de edad!");
+        } else {
+            System.out.println("Es menor de edad!");
+        }
+
+        System.out.println(personaConTodosLosAtributos);
+    }
+}
