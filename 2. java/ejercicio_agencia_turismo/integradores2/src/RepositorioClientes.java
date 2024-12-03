9f84ea9e59967cd2d6f1cfb477de30d0e82d0283
elPeiretti <tomas.peiretti@mercadolibre.com>
2024-12-03 14:25:38 -0300

    agrega solucion problema agencia turismo java

---
+++ 2. java/ejercicio_agencia_turismo/integradores2/src/RepositorioClientes.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RepositorioClientes {
+
+    private HashMap<Integer, Cliente> clientes;
+
+    public RepositorioClientes() {
+        this.clientes = new HashMap<>();
+    }
+
+    public HashMap<Integer, Cliente> getClientes() {
+        return clientes;
+    }
+
+    public Localizador buscarLocalizador(){
+        return null;
+    }
+
+    public double  aplicarDescuento(Cliente cli){
+
+        double total = 0;
+        boolean aplicaDescuentoExtra = cli.getLocalizadores().size() >= 2;
+
+        for(Localizador local: cli.getLocalizadores()){
+            if(local.getReservas().stream().map(r -> r.getTipoReserva()).collect(Collectors.toSet()).size() == 4){
+                //if(local.getReservas().stream().map(r -> r.getTipoReserva()).collect(Collectors.toSet()).containsAll(List.of()))
+                total += local.getTotal() * 0.9;
+            }else if(local.getReservas().stream().map(r -> r.getTipoReserva()).filter( t -> t.equals("hotel")).count() >= 2 ||
+                    local.getReservas().stream().map(r -> r.getTipoReserva()).filter( t -> t.equals("boletos")).count() >= 2){
+                total += local.getTotal() * 0.95;
+            }else {
+                total += local.getTotal();
+            }
+        }
+
+        if(aplicaDescuentoExtra)
+            return  total * 0.95;
+        else  return total;
+    }
+
+    public  int totalLocalizadores(){
+
+        int totalLocalizadores = 0;
+
+        for (Cliente cli: this.clientes.values()){
+            totalLocalizadores +=  cli.getLocalizadores().size();
+        }
+
+        return totalLocalizadores;
+    }
+
+    public  int totalReservas(){
+
+        int totalReservas = 0;
+
+        for (Cliente cli: this.clientes.values()){
+            for (Localizador local: cli.getLocalizadores()){
+                totalReservas += local.getReservas().size();
+            }
+
+        }
+
+        return totalReservas;
+    }
+
+    public Map<String, List<Reserva>> diccionarioReservas() {
+
+        Map<String, List<Reserva>> mapaReservas = Map.of("hotel", new ArrayList(), "boletos", new ArrayList(),
+                "comida", new ArrayList<>(), "transporte", new ArrayList<>());
+
+        for (Cliente cli : this.clientes.values()) {
+            for (Localizador local : cli.getLocalizadores()) {
+                for (Reserva res : local.getReservas()) {
+                    mapaReservas.get(res.getTipoReserva()).add(res);
+                }
+            }
+        }
+
+        return mapaReservas;
+    }
+
+    public void guardarCliente(Cliente cli){
+        this.clientes.put(cli.getDNI(), cli);
+    }
+
+    public double calcularVentas() {
+        return clientes.values().stream()
+                .map(this::aplicarDescuento)
+                .reduce(Double::sum)
+                .orElse(0d);
+    }
+
+    public double calcularPromedioVentas() {
+        int cantClientes = clientes.values().size();
+        if (cantClientes == 0)
+            return 0;
+        return calcularVentas()/cantClientes;
+    }
+
+}
