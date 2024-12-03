74a1af10874940723f59ed838e9ebb4a5c20288b
Caroschwab97 <caroschwab97@gmail.com>
2024-12-03 17:51:32 -0300

    Subo resolución de ejercico de guarda ropa

---
+++ 2. java/ejercicios/integradores/ropero/Prenda.java
@@ -0,0 +1,22 @@
+package ejercicios.integradores.ropero;
+
+public class Prenda {
+    private int id;
+    private String marca;
+    private String modelo;
+
+    public Prenda(int id, String marca, String modelo) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prenda{" +
+                "id=" + id +
+                ", marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                '}';
+    }
+}
