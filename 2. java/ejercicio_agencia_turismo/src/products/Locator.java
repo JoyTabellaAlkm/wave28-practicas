241271df2d7848836b4aa5a38c928c6a434b3344
Ariana Manusovich <arimanuso@gmail.com>
2024-12-03 18:08:47 -0300

    WIP  - Ejercicio agencia turismo

---
+++ 2. java/ejercicio_agencia_turismo/src/products/Locator.java
@@ -0,0 +1,15 @@
+package products;
+import client.Client;
+import java.util.List;
+
+public class Locator {
+
+    public Locator(List<Product> productList, Client owner){
+        this.productList = productList;
+    }
+
+    public Client owner;
+    List<Product> productList;
+    double totalPrice;
+
+}
