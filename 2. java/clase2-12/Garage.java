198e00656f916825d7e94e005ec568ed7db29d70
Lara <lrfrryr@gmail.com>
2024-12-04 11:18:01 -0300

    Agrega y reorganiza archivos para clases del 28, 29, 2 y 3

---
+++ 2. java/clase2-12/Garage.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import java.util.List;
+
+public class Garage {
+
+    private int id;
+    private List<Vehiculo> listaVehiculos;
+
+
+    public Garage(int id, List<Vehiculo> listaVehiculos) {
+        this.id = id;
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Vehiculo> getListaVehiculos() {
+        return listaVehiculos;
+    }
+
+    public void setListaVehiculos(List<Vehiculo> listaVehiculos) {
+        this.listaVehiculos = listaVehiculos;
+    }
+
+    @Override
+    public String toString() {
+        return "Garage{" +
+                "id=" + id +
+                ", listaVehiculos=" + listaVehiculos +
+                '}';
+    }
+}
