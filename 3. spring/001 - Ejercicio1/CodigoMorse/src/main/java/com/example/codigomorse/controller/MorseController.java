98a55ea99a73137e91ab787753755ed8afdcb1fc
andresuarique <andresuarique@gmail.com>
2024-12-04 16:36:21 -0500

    Ajustes Ejercicio Codigo Morse

--- 3. spring/001 - Ejercicio1/CodigoMorse/src/main/java/com/example/codigomorse/controller/MorseController.java
+++ 3. spring/001 - Ejercicio1/CodigoMorse/src/main/java/com/example/codigomorse/controller/MorseController.java
@@ -1,14 +1,11 @@
 package com.example.codigomorse.controller;
 
 import com.example.codigomorse.servicio.TraducirService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
 @RestController
@@ -16,10 +13,15 @@ import java.util.stream.Collectors;
 public class MorseController {
     private TraducirService service = new TraducirService();
 
-    @GetMapping("/texto")
-    public String traducirCodigo(@RequestParam String codigo) {
+    @GetMapping("/decodificar")
+    public String decodificarCodigo(@RequestParam String codigo) {
 
-        return service.separarPalabras(codigo).stream().collect(Collectors.joining(" "));
+        return service.decodificarPalabras(codigo).stream().collect(Collectors.joining(" "));
+    }
+
+    @GetMapping("/codificar")
+    public String traducirTexto(@RequestParam String texto){
+        return service.codificarPalabras(texto.toUpperCase());
     }
 
 
--- 3. spring/001 - Ejercicio1/CodigoMorse/src/main/java/com/example/codigomorse/servicio/TraducirService.java
+++ 3. spring/001 - Ejercicio1/CodigoMorse/src/main/java/com/example/codigomorse/servicio/TraducirService.java
@@ -7,59 +7,61 @@ import java.util.stream.Collectors;
 
 @Service
 public class TraducirService {
-    public String traducir(String codigo) {
-        Map<String, String> morseToTexto = new HashMap<String, String>() {{
-            put(".-", "A");
-            put("-...", "B");
-            put("-.-.", "C");
-            put("-..", "D");
-            put(".", "E");
-            put("..-.", "F");
-            put("--.", "G");
-            put("....", "H");
-            put("..", "I");
-            put(".---", "J");
-            put("-.-", "K");
-            put(".-..", "L");
-            put("--", "M");
-            put("-.", "N");
-            put("---", "O");
-            put(".--.", "P");
-            put("--.-", "Q");
-            put(".-.", "R");
-            put("...", "S");
-            put("-", "T");
-            put("..-", "U");
-            put("...-", "V");
-            put(".--", "W");
-            put("-..-", "X");
-            put("-.--", "Y");
-            put("--..", "Z");
-            put("-----", "0");
-            put(".----", "1");
-            put("..---", "2");
-            put("...--", "3");
-            put("....-", "4");
-            put(".....", "5");
-            put("-....", "6");
-            put("--...", "7");
-            put("---..", "8");
-            put("----.", "9");
-            put(".-.-.-", ".");
-            put("--..--", ",");
-            put("..--..", "?");
-            put("-.-.--", "!");
-            put("-....-", "-");
-            put(".----.", "'");
-            put("-..-.", "/");
-            put("-.--.", "(");
-            put("-.--.-", ")");
-            put(".-.-.", "+");
-            put("-...-", "=");
-            put(".-..-.", "\"");
-            put("...-..-", "$");
-            put(".--.-.", "@");
-        }};
+    Map<String, String> morseToTexto = new HashMap<String, String>() {{
+        put(".-", "A");
+        put("-...", "B");
+        put("-.-.", "C");
+        put("-..", "D");
+        put(".", "E");
+        put("..-.", "F");
+        put("--.", "G");
+        put("....", "H");
+        put("..", "I");
+        put(".---", "J");
+        put("-.-", "K");
+        put(".-..", "L");
+        put("--", "M");
+        put("-.", "N");
+        put("---", "O");
+        put(".--.", "P");
+        put("--.-", "Q");
+        put(".-.", "R");
+        put("...", "S");
+        put("-", "T");
+        put("..-", "U");
+        put("...-", "V");
+        put(".--", "W");
+        put("-..-", "X");
+        put("-.--", "Y");
+        put("--..", "Z");
+        put("-----", "0");
+        put(".----", "1");
+        put("..---", "2");
+        put("...--", "3");
+        put("....-", "4");
+        put(".....", "5");
+        put("-....", "6");
+        put("--...", "7");
+        put("---..", "8");
+        put("----.", "9");
+        put(".-.-.-", ".");
+        put("--..--", ",");
+        put("..--..", "?");
+        put("-.-.--", "!");
+        put("-....-", "-");
+        put(".----.", "'");
+        put("-..-.", "/");
+        put("-.--.", "(");
+        put("-.--.-", ")");
+        put(".-.-.", "+");
+        put("-...-", "=");
+        put(".-..-.", "\"");
+        put("...-..-", "$");
+        put(".--.-.", "@");
+        put("  ", " ");// Para agregar espacios entre palabras
+    }};
+    public String decodificarPalabra(String codigo) {
+
         List<String> traducido = new ArrayList<>();
         Arrays.stream(codigo.split(" ")).forEach(p -> {
             System.out.println(morseToTexto.get(p));
@@ -69,16 +71,34 @@ public class TraducirService {
         return traducido.stream().collect(Collectors.joining(""));
     }
 
-    public List<String> separarPalabras(String codigo) {
-        List<String> palabrasTraducidas = new ArrayList<>();
+    public List<String> decodificarPalabras(String codigo) {
+        List<String> palabrasDecodificadas = new ArrayList<>();
         if (codigo.contains("   ")) {
             String[] palabras = codigo.split("   ");
             Arrays.stream(palabras).forEach(p -> {
-                palabrasTraducidas.add(traducir(p));
+                palabrasDecodificadas.add(decodificarPalabra(p));
             });
-            return palabrasTraducidas;
+            return palabrasDecodificadas;
         }
-        palabrasTraducidas.add(traducir(codigo));
-        return palabrasTraducidas;
+        palabrasDecodificadas.add(decodificarPalabra(codigo));
+        return palabrasDecodificadas;
     }
+    public String codificarPalabras(String texto){
+        Map<String, String> textoToMorse = new HashMap<String, String>();
+        morseToTexto.forEach((i,value)-> textoToMorse.put(value,i));
+        String palabraCodificada = "";
+
+        //Codifica cada caracter
+        for(int i = 0; i< texto.length(); i++){
+            System.out.println(textoToMorse.get(String.valueOf(texto.charAt(i))));
+            palabraCodificada += textoToMorse.get(String.valueOf(texto.charAt(i)));
+
+            //Para agregar espacios entre letras
+            if(!String.valueOf(texto.charAt(i)).equals(" ")){
+                palabraCodificada+=" ";
+            }
+        }
+        return palabraCodificada;
+    }
+
 }
