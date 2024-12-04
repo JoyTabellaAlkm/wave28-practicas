64ce9f7d87ff1a5f1e090c8b3f202ca6bddc1985
elPeiretti <tomas.peiretti@mercadolibre.com>
2024-12-04 14:50:09 -0300

    arregla solucion nums romanos spring

--- 3. spring/ejercicio_romanos/src/main/java/com/bootcamp/ejercicio1/RomanController.java
+++ 3. spring/ejercicio_romanos/src/main/java/com/bootcamp/ejercicio1/RomanController.java
@@ -13,10 +13,14 @@ public class RomanController {
         int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
         String[] chars = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-        for(int i=0; i<values.length; i++) {
+        int i = 0;
+        while(number > 0) {
             if (number >= values[i]) {
                 stringBuilder.append(chars[i]);
                 number = number - values[i];
+            }
+            else {
+                i++;
             }
         }
 
