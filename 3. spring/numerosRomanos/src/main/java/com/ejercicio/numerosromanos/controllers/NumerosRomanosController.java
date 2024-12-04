11efafecd8aa5833e3fb6a84d3d383830c434414
astrid21 <astridmalamud@gmail.com>
2024-12-04 17:12:46 -0300

    corregir servicio

--- 3. spring/numerosRomanos/src/main/java/com/ejercicio/numerosromanos/controllers/NumerosRomanosController.java
+++ 3. spring/numerosRomanos/src/main/java/com/ejercicio/numerosromanos/controllers/NumerosRomanosController.java
@@ -1,18 +1,19 @@
 package com.ejercicio.numerosromanos.controllers;
 
+import com.ejercicio.numerosromanos.services.NumerosRomanosService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ejercicio.numerosromanos.services.numerosRomanos;
-
 @RestController
 @RequestMapping("/romanos")
 public class NumerosRomanosController {
 
+    private final NumerosRomanosService servicio = new NumerosRomanosService();
+
     @GetMapping("numero/{number}")
     public String convertirDecimalARomano(@PathVariable Integer number){
-        return numerosRomanos.aNumeroRomano(number);
+        return servicio.aNumeroRomano(number);
     }
 }
--- 3. spring/numerosRomanos/src/main/java/com/ejercicio/numerosromanos/services/NumerosRomanosService.java
+++ 3. spring/numerosRomanos/src/main/java/com/ejercicio/numerosromanos/services/NumerosRomanosService.java
@@ -3,11 +3,11 @@ package com.ejercicio.numerosromanos.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public class numerosRomanos {
+public class NumerosRomanosService {
     private static final int[] numbersToCompare = new int[]{1000,900,500,400,100,90,50,40,10, 9, 5, 4, 1};
     private static final String[] romanNumbers = new String[]{"M","CM","D","CD","C","XC","L","XL", "X", "IX", "V", "IV", "I"};
 
-    public static String aNumeroRomano(Integer number) {
+    public String aNumeroRomano(Integer number) {
         StringBuilder romanNumber = new StringBuilder();
         for (int i = 0; i < numbersToCompare.length; i++)
             for (;number >= numbersToCompare[i]; number -= numbersToCompare[i])
