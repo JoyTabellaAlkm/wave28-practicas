20a3be8637f17bc994e011483437f0366d92d13d
juanbianchi <juanbianchi@icloud.com>
2024-12-04 12:07:11 -0300

    feat: Add 'ropero' exercise

---
+++ 2. java/Ejercicios-Integradores/P2-vivo/ropero/src/GuardaRopa.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GuardaRopa {
+
+    private int id;
+    private Integer idMap;
+    private Map<Integer, List<Prenda>> prendas;
+
+    public GuardaRopa(int id) {
+        this.id = id;
+        this.prendas = new HashMap<>();
+        this.idMap = 0;
+    }
+    public Integer guardarPrendas(List<Prenda> listaDePrenda) {
+        prendas.put(idMap,listaDePrenda);
+        return idMap++;
+    }
+    public void mostrarPrendas() {
+        prendas.forEach((id, prendas) -> System.out.println("Número: " + id + ", Prendas: " + prendas));
+    }
+    public List<Prenda> devolverPrendas(Integer numero) {
+        return prendas.get(numero);
+    }
+}
