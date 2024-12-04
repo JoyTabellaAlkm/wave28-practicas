e3ee976929f9ba253e9c59bf937687bd517a5791
renjirock <mauriciokode@gmail.com>
2024-12-03 20:01:35 -0500

    feat: add dakar

---
+++ 2. java/dakar/clases/Carrera.java
@@ -0,0 +1,94 @@
+package clases;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Carrera {
+    private String nombre;
+    private double distancia;
+    private double premioEnDolares;
+    private int cantidadDeVehiculosPermitidos;
+    private List<Vehiculo> vehiculos;
+    private SocorristaAuto socorristaAuto = new SocorristaAuto();
+    private SocorristaMoto socorristaMoto = new SocorristaMoto();
+
+    public Carrera(String nombre, double distancia, double premioEnDolares, int cantidadDeVehiculosPermitidos, List<Vehiculo> vehiculos){
+        this.nombre = nombre;
+        this.distancia = distancia;
+        this.premioEnDolares = premioEnDolares;
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+        this.vehiculos = vehiculos;
+    }
+
+    public List<Vehiculo> getVehiculos() {
+        return vehiculos;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public double getPremioEnDolares() {
+        return premioEnDolares;
+    }
+
+    public int getCantidadDeVehiculosPermitidos() {
+        return cantidadDeVehiculosPermitidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setVehiculos(List<Vehiculo> vehiculos) {
+        this.vehiculos = vehiculos;
+    }
+
+    public void setCantidadDeVehiculosPermitidos(int cantidadDeVehiculosPermitidos) {
+        this.cantidadDeVehiculosPermitidos = cantidadDeVehiculosPermitidos;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setPremioEnDolares(double premioEnDolares) {
+        this.premioEnDolares = premioEnDolares;
+    }
+
+    public void darDeAltaAuto(double velocidad, double aceleracion,double anguloDeGiro,String patente){
+        if (this.cantidadDeVehiculosPermitidos >= this.vehiculos.size()){
+            this.vehiculos.add(new Auto(velocidad, aceleracion, anguloDeGiro, patente));
+        }
+    }
+
+    public void darDeAltaMoto(double velocidad, double aceleracion,double anguloDeGiro,String patente){
+        if (this.cantidadDeVehiculosPermitidos >= this.vehiculos.size()){
+            this.vehiculos.add(new Moto(velocidad, aceleracion, anguloDeGiro, patente));
+        }
+    }
+    public void eliminarVehiculo(Vehiculo vehículo){
+        this.vehiculos = this.vehiculos.stream().filter((v)-> !v.equals(vehículo)).toList();
+    }
+    public void eliminarVehiculoConPatente(String unaPatente){
+        this.vehiculos = this.vehiculos.stream().filter((v)-> !v.getPatente().equals(unaPatente)).toList();
+    }
+
+    public Vehiculo winner(){
+        return this.vehiculos.stream().max(Comparator.comparing(Vehiculo::calcularVelocidad)).orElse(null);
+    }
+
+    public void socorrerAuto(String patente) {
+        Vehiculo auto = this.vehiculos.stream().filter(v-> v.getPatente().equals(patente)).findFirst().orElse(null);
+        socorristaAuto.socorrer((Auto) auto);
+    }
+
+    public void socorrerMoto(String patente){
+        Vehiculo auto = this.vehiculos.stream().filter(v-> v.getPatente().equals(patente)).findFirst().orElse(null);
+        socorristaMoto.socorrer((Moto) auto);
+    }
+}
---
+++ 2. java/dakar/clases/Vehiculo.java
@@ -0,0 +1,65 @@
+package clases;
+
+public class Vehiculo {
+    private double velocidad;
+    private double aceleracion;
+    private double anguloDeGiro;
+    private String patente;
+    private double peso;
+    private int ruedas;
+
+    public  Vehiculo(double velocidad, double aceleracion, double anguloDeGiro, String patente, double peso, int ruedas){
+        this.velocidad = velocidad;
+        this.aceleracion = aceleracion;
+        this.anguloDeGiro = anguloDeGiro;
+        this.patente = patente;
+        this.peso = peso;
+        this.ruedas = ruedas;
+    }
+
+    public void setAceleracion(double aceleracion) {
+        this.aceleracion = aceleracion;
+    }
+
+    public void setAnguloDeGiro(double anguloDeGiro) {
+        this.anguloDeGiro = anguloDeGiro;
+    }
+
+    public void setPatente(String patente) {
+        this.patente = patente;
+    }
+
+    public void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+
+    public double getAceleracion() {
+        return aceleracion;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getVelocidad() {
+        return velocidad;
+    }
+
+    public double getAnguloDeGiro() {
+        return anguloDeGiro;
+    }
+
+    public int getRuedas() {
+        return ruedas;
+    }
+
+    public String getPatente() {
+        return patente;
+    }
+
+    public double calcularVelocidad(){
+        double velocidad = (this.getVelocidad()*(this.getAceleracion()*0.5))/(this.getAnguloDeGiro()*(this.getPeso()-(this.getRuedas()*100)));
+        return velocidad;
+    }
+}
