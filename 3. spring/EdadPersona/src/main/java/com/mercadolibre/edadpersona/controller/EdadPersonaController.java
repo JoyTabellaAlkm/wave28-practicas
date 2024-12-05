62999fb001d377c87094ca153439a1cc7f78b710
Choolito <juanignacioabarca2020@gmail.com>
2024-12-05 14:04:26 -0300

    Se agrega la actividad de la edad de las personas en base a la fecha

---
+++ 3. spring/EdadPersona/src/main/java/com/mercadolibre/edadpersona/controller/EdadPersonaController.java
@@ -0,0 +1,34 @@
+package com.mercadolibre.edadpersona.controller;
+
+import com.mercadolibre.edadpersona.service.IEdadPersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EdadPersonaController {
+
+    @Autowired
+    private IEdadPersonaService edadPersonaService;
+
+    @GetMapping("/{dia}/{mes}/{anio}")
+    public String devolverEdadPersona(@PathVariable int dia,
+                                   @PathVariable int mes,
+                                   @PathVariable int anio){
+
+        return edadPersonaService.calcularEdadPersona(dia, mes, anio);
+    }
+
+    //Con try catch
+    @GetMapping("/edad/{dia}/{mes}/{anio}")
+    public String devolverEdadPersona1(@PathVariable int dia,
+                                      @PathVariable int mes,
+                                      @PathVariable int anio){
+
+        int edad = edadPersonaService.calcularEdadPersona1(dia, mes, anio);
+        return edad == -1 ? "Error: Mal ingresado en la fecha" : String.valueOf(edad);
+    }
+
+}
---
+++ 3. spring/EdadPersona/src/main/java/com/mercadolibre/edadpersona/service/IEdadPersonaService.java
@@ -0,0 +1,6 @@
+package com.mercadolibre.edadpersona.service;
+
+public interface IEdadPersonaService {
+    public String calcularEdadPersona(int dia, int mes, int anio);
+    public Integer calcularEdadPersona1(int dia, int mes, int anio);
+}
