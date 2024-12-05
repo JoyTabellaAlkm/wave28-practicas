92fc934cd6d1ce41ba7213c72ec75f21675c9624
vilo0812 <gabriel.viloria0812@gmail.com>
2024-12-05 09:50:18 -0500

    EjercicioEdades

---
+++ 3. spring/EdadPersona/src/main/java/ejercicios/EdadPersona/controllers/CalAgeController.java
@@ -0,0 +1,19 @@
+package ejercicios.EdadPersona.controllers;
+
+import ejercicios.EdadPersona.services.CalculateService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("person")
+public class CalAgeController {
+
+
+    @GetMapping("/calAge/{day}/{month}/{year}")
+    public int calAge(@PathVariable int day,@PathVariable int month,@PathVariable int year
+    ){
+        return CalculateService.calculateAge(day,month,year);
+    }
+}
---
+++ 3. spring/EdadPersona/src/main/java/ejercicios/EdadPersona/services/CalculateService.java
@@ -0,0 +1,18 @@
+package ejercicios.EdadPersona.services;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class CalculateService {
+
+    public static int calculateAge(int day, int month, int year){
+        int result = 0;
+        LocalDate fecha = LocalDate.of(year,month,day);
+        LocalDate fechaActual = LocalDate.now();
+
+        Period diferencia = Period.between(fecha, fechaActual);
+        return diferencia.getYears();
+
+    }
+
+}
