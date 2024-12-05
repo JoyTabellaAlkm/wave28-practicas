87aa853520b660b50b8e682c0ad006de464594aa
Karina Sierra Velandia <kkarinasierrav@gmail.com>
2024-12-05 08:04:12 -0500

    ejercicioDakar

---
+++ 2. java/EjercicioDakar/Auto.java
@@ -0,0 +1,5 @@
+public class Auto extends Vehiculo{
+    public Auto(Double velocidad, Double aceleracion, Double anguloGiro, String patente) {
+        super(velocidad, aceleracion, anguloGiro, patente,1000.00,4);
+    }
+}
