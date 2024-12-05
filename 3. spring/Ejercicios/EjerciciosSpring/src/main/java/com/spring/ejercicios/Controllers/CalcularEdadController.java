e10207e6d95281b5ab8baaacb632f8ac9ba5dc96
Caroschwab97 <caroschwab97@gmail.com>
2024-12-05 12:23:15 -0300

    hago refactor de carpetas

---
+++ 3. spring/Ejercicios/EjerciciosSpring/src/main/java/com/spring/ejercicios/Controllers/CalcularEdadController.java
@@ -0,0 +1,18 @@
+package com.spring.ejercicios.Controllers;
+
+import com.spring.ejercicios.Services.CalculadoraEdadService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.spring.ejercicios.Services.CalculadoraEdadService.calcularEdad;
+
+@RestController
+public class CalcularEdadController {
+
+    @GetMapping("/calculadoraEdad/{dia}/{mes}/{anio}")
+    public int getEdad(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+        return calcularEdad(dia, mes, anio);
+    }
+}
---
+++ 3. spring/Ejercicios/EjerciciosSpring/src/main/java/com/spring/ejercicios/Services/CalculadoraEdadService.java
@@ -0,0 +1,17 @@
+package com.spring.ejercicios.Services;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@Service
+public class CalculadoraEdadService {
+
+    public static int calcularEdad(int dia, int mes, int anio){
+        LocalDate fechaHoy = LocalDate.now();
+        LocalDate fechaelegida = LocalDate.of(anio,mes,dia);
+        Period period = Period.between(fechaelegida, fechaHoy);
+        return period.getYears();
+    }
+}
