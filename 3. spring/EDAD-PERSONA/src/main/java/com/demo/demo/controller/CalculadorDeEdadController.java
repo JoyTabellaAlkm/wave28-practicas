5c17c3ee87e4331d251b1313636b3d7c1f7e613a
peyk0v <peykovn@gmail.com>
2024-12-05 12:09:18 -0300

    subo ejercicio de calcular la edad de una persona

---
+++ 3. spring/EDAD-PERSONA/src/main/java/com/demo/demo/controller/CalculadorDeEdadController.java
@@ -0,0 +1,26 @@
+package com.demo.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+@RestController
+@RequestMapping("/")
+public class CalculadorDeEdadController {
+
+    @GetMapping("{dia}/{mes}/{anio}")
+    public int calcularEdad(
+            @PathVariable int dia,
+            @PathVariable int mes,
+            @PathVariable int anio
+    ) {
+        LocalDate fechaDeNacimiento = LocalDate.of(anio, mes, dia);
+        Period edad = Period.between(fechaDeNacimiento, LocalDate.now());
+        return edad.getYears();
+    }
+}
