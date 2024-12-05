d7cd026f87f13a0f447e1314a340224b754885ff
Caroschwab97 <caroschwab97@gmail.com>
2024-12-05 12:23:15 -0300

    hago refactor de carpetas

---
+++ 3. spring/Ejercicios/EjerciciosSpring/src/main/java/com/spring/ejercicios/Controllers/CodigoMorseController.java
@@ -0,0 +1,16 @@
+package com.spring.ejercicios.Controllers;
+
+import com.spring.ejercicios.Services.CodigoMorseService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CodigoMorseController {
+    CodigoMorseService morseService = new CodigoMorseService();
+
+    @GetMapping("/convertir")
+    public String convertirMorseAEspañol(@RequestParam String morse) {
+        return morseService.convertirMorseATexto(morse);
+    }
+}
