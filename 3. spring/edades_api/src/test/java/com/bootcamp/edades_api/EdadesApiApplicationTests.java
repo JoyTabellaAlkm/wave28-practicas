7cb34bf301eca50fe0a77063999621b6fae1bfb8
LucasCABJ <lucascaraballo15@gmail.com>
2024-12-05 12:02:02 -0300

    Se realizó el ejercicio API calculadora de edades

---
+++ 3. spring/edades_api/src/test/java/com/bootcamp/edades_api/EdadesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.edades_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdadesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
