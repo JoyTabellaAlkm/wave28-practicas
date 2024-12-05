e0a5560ce4f5547b7302aea6f9d5a2c9cd270206
redflox <swsbmm@gmail.com>
2024-12-05 17:12:45 -0500

    feat: added morse

---
+++ 3. spring/3. Introducción a Spring - P2 - VIVO/morse/controllers/MorseRestController.java
@@ -0,0 +1,29 @@
+package com.example.morse.controllers;
+
+import com.example.morse.services.MorseCode;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/demo/v0.2", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MorseRestController {
+
+    @GetMapping("hello")
+    public String hello(){
+        return "hola mundo";
+    }
+
+    @GetMapping("/convert")
+    public String convert(@RequestParam MorseCode codes){
+        return codes.convert();
+    }
+    @GetMapping("/convert/{codes}")
+    public String convertByPath(@PathVariable MorseCode codes){
+        return codes.convert();
+    }
+
+    @PostMapping("/convert")
+    public String convertByPost(@RequestBody MorseCode codes){
+        return codes.convert();
+    }
+}
