e8bab6f34419fb84ffa2147dab0110f7dcd940b0
Caroschwab97 <caroschwab97@gmail.com>
2024-12-05 17:53:08 -0300

    pequeños cambios al proyecto covid

--- 3. spring/Ejercicios/EjerciciosSpring/src/main/java/com/spring/ejercicios/controllers/CovidController.java
+++ 3. spring/Ejercicios/EjerciciosSpring/src/main/java/com/spring/ejercicios/controllers/CovidController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,4 +33,5 @@ public class CovidController {
     public ResponseEntity<List<personaDTO>> findRiskPersons() {
         return ResponseEntity.ok(service.personasRiesgo());
     }
+
 }
