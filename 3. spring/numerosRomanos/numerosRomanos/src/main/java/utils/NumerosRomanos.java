d7983579a0b8079a9b14d1f767259b1cc66ce451
juanbianchi <juanbianchi@icloud.com>
2024-12-05 09:11:21 -0300

    feat: Merge from main

--- 3. spring/numerosRomanos/numerosRomanos/src/main/java/utils/NumerosRomanos.java
+++ 3. spring/numerosRomanos/numerosRomanos/src/main/java/utils/NumerosRomanos.java
@@ -1,4 +1,23 @@
 package utils;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class NumerosRomanos {
+
+    public static Map<Integer, String> numerosRomanos = Stream.of(new Object[][] {
+            {1, "I"},
+            {5, "V"},
+            {10, "X"},
+            {50, "L"},
+            {100, "C"},
+            {500, "D"},
+            {1000, "M"}
+    }).collect(Collectors.toMap(p -> (Integer) p[0], p -> (String) p[1]));
+
+    public static Integer devolverDecimal(Integer numeroDecimal) {
+
+    }
 }
