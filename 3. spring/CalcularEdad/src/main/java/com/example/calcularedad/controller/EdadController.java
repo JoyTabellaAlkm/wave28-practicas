52859c145669656a2bbc242684995cdf93fbeb54
David Moreno <david.dmoreno@mercadolibre.com.co>
2024-12-05 10:17:06 -0500

    Update (CalcularEdad) Se añade excepciones para fechas invalidas

--- 3. spring/CalcularEdad/src/main/java/com/example/calcularedad/controller/EdadController.java
+++ 3. spring/CalcularEdad/src/main/java/com/example/calcularedad/controller/EdadController.java
@@ -1,4 +1,22 @@
 package com.example.calcularedad.controller;
 
+import com.example.calcularedad.service.EdadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/edad")
 public class EdadController {
+
+    @Autowired
+    private EdadService edadService;
+
+    @GetMapping("{dia}/{mes}/{anio}")
+    public String calcularEdad(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio){
+
+        return edadService.calcularEdad(dia, mes, anio);
+    }
 }
--- 3. spring/CalcularEdad/src/main/java/com/example/calcularedad/service/EdadService.java
+++ 3. spring/CalcularEdad/src/main/java/com/example/calcularedad/service/EdadService.java
@@ -1,4 +1,36 @@
 package com.example.calcularedad.service;
 
+import org.springframework.stereotype.Service;
+
+import java.time.DateTimeException;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Service
 public class EdadService {
+    public  String calcularEdad(Integer dia, Integer mes, Integer anio){
+
+        LocalDate fechaActual = LocalDate.now();
+        LocalDate fechaNacimiento;
+
+        Boolean cumplioAnios = false;
+
+        try{
+            fechaNacimiento = LocalDate.of(anio, mes, dia);
+        }catch (DateTimeException e){
+            return "Fecha invalida";
+        }
+
+        if(fechaActual.getDayOfMonth() > fechaNacimiento.getDayOfMonth()){
+            cumplioAnios = true;
+        }else if(fechaActual.getDayOfMonth() == fechaNacimiento.getDayOfMonth()){
+            if(fechaActual.getDayOfMonth() >= fechaNacimiento.getDayOfMonth()){
+                cumplioAnios = true;
+            }
+        }
+
+        Integer edad = cumplioAnios ? fechaActual.getYear() - fechaNacimiento.getYear() : (fechaActual.getYear() - fechaNacimiento.getYear()) -1;
+
+        return "Su edad actual es: " + edad;
+    }
 }
