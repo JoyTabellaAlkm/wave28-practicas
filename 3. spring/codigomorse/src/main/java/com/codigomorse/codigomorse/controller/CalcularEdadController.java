85469e9a13764ec4bbefb2cfe1fdc1df4720dd7e
fcastaldoback <fercastaldo@hotmail.com>
2024-12-05 12:05:20 -0300

    Ejercicio calcular edad

---
+++ 3. spring/codigomorse/src/main/java/com/codigomorse/codigomorse/controller/CalcularEdadController.java
@@ -0,0 +1,18 @@
+package com.codigomorse.codigomorse.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.codigomorse.codigomorse.service.CalcularEdadService.calcularEdad;
+
+
+@RestController
+public class CalcularEdadController {
+    @GetMapping("/calculadoraEdad/{dia}/{mes}/{anio}")
+    public int getEdad(@PathVariable int dia, @PathVariable int mes, @PathVariable int anio) {
+        return calcularEdad(dia, mes, anio);
+    }
+
+
+}
