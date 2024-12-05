f98d82bcc73572e34760e2e1b7279d7bde0ab660
MarianoChun <marianochun01@gmail.com>
2024-12-05 12:21:21 -0300

    Add validacion fecha posterior y cambio tipo dato devolucion endpoint calculo edad

--- 3. spring/calculo-edad/src/main/java/com/bootcamp/calculoedad/controller/EdadController.java
+++ 3. spring/calculo-edad/src/main/java/com/bootcamp/calculoedad/controller/EdadController.java
@@ -5,20 +5,22 @@ import com.bootcamp.calculoedad.service.EdadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("edad")
 public class EdadController {
     @Autowired
     EdadService edadService;
 
 
-    @GetMapping("edad/{dia}/{mes}/{anio}")
-    public Integer calcularEdad(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio) {
+    @GetMapping("{dia}/{mes}/{anio}")
+    public String calcularEdad(@PathVariable Integer dia, @PathVariable Integer mes, @PathVariable Integer anio) {
         try {
             return edadService.calcularEdad(dia, mes, anio);
         } catch (Exception e) {
-            return 0;
+            return e.getMessage();
         }
     }
 }
--- 3. spring/calculo-edad/src/main/java/com/bootcamp/calculoedad/service/EdadService.java
+++ 3. spring/calculo-edad/src/main/java/com/bootcamp/calculoedad/service/EdadService.java
@@ -11,18 +11,24 @@ import java.time.temporal.TemporalUnit;
 @Service
 public class EdadService {
 
-    public Integer calcularEdad(Integer dia, Integer mes, Integer anio) {
+    public String calcularEdad(Integer dia, Integer mes, Integer anio) {
         int edadCalculada;
 
         try {
             LocalDate fechaIngresada = LocalDate.of(anio, mes, dia);
             LocalDate fechaActual = LocalDate.now();
+
+            if (fechaIngresada.isAfter(fechaActual)) {
+                throw new IllegalArgumentException("No se puede ingresar una fecha posterior a la actual");
+            }
             edadCalculada = Period.between(fechaIngresada, fechaActual).getYears();
 
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
         } catch (Exception e) {
             throw new IllegalArgumentException("Ingrese una fecha valida");
         }
 
-        return  edadCalculada;
+        return String.valueOf(edadCalculada);
     }
 }
