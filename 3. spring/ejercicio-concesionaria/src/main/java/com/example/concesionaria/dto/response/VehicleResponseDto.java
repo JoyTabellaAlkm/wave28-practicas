0cb30e7b27baa8f4036a331565e5b0ee4ee513f8
SantiMar00 <santimarinio2000@gmail.com>
2024-12-06 19:10:11 -0300

    add ejercicio-concesionaria

---
+++ 3. spring/ejercicio-concesionaria/src/main/java/com/example/concesionaria/dto/response/VehicleResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.concesionaria.dto.response;
+
+import com.example.concesionaria.model.Service;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class VehicleResponseDto {
+
+    private String brand;
+    private String model;
+    private String manufacturingDate;
+    private String numberOfKilometers;
+    private String doors;
+    private String price;
+    private String currency;
+    private String countOfOwners;
+}
---
+++ 3. spring/ejercicio-concesionaria/src/main/java/com/example/concesionaria/model/Vehicle.java
@@ -0,0 +1,24 @@
+package com.example.concesionaria.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Vehicle {
+
+    private String brand;
+    private String model;
+    private String manufacturingDate;
+    private String numberOfKilometers;
+    private String doors;
+    private String price;
+    private String currency;
+    private List<Service> servicios;
+    private String countOfOwners;
+
+}
