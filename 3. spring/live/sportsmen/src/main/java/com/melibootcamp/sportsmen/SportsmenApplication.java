c2d5198c0218b7ef760c72c310a97008619942fb
lvingman <r_martin@tutanota.com>
2024-12-06 15:54:46 -0300

    feat: spring: started star-wars exercise (NOT FINISHED)

---
+++ 3. spring/live/sportsmen/src/main/java/com/melibootcamp/sportsmen/SportsmenApplication.java
@@ -0,0 +1,13 @@
+package com.melibootcamp.sportsmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsmenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsmenApplication.class, args);
+    }
+
+}
---
+++ 3. spring/live/sportsmen/src/test/java/com/melibootcamp/sportsmen/SportsmenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melibootcamp.sportsmen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsmenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
