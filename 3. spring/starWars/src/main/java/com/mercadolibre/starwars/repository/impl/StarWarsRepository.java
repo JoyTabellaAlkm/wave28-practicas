33db3b4e644a55a4350e0a3761b43702355f5ec7
guille-kalniker <guillermo.kalniker@mercadolibre.com>
2024-12-08 01:32:23 -0300

    Subo ej de starWars con obtención de datos de archivo json

--- 3. spring/starWars/src/main/java/com/mercadolibre/starwars/repository/impl/StarWarsRepository.java
+++ 3. spring/starWars/src/main/java/com/mercadolibre/starwars/repository/impl/StarWarsRepository.java
@@ -1,9 +1,12 @@
 package com.mercadolibre.starwars.repository.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mercadolibre.starwars.model.Character;
 import com.mercadolibre.starwars.repository.IStarWarsRepository;
 import org.springframework.stereotype.Repository;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,13 +16,14 @@ public class StarWarsRepository implements IStarWarsRepository {
     private List<Character> characters = new ArrayList<>();
 
     public StarWarsRepository() {
-        Character luke = new Character("Luke Skywalker",2,83,"rubio","blanco","verde", "20/02/1983", "masculino", "tierra", "humano");
-        Character darthVader = new Character("Darth Vader",2,130,"marron","oscura","marron", "12/04/1934", "masculino", "saturno", "humano");
-        Character darthMaul = new Character("Darth Maul",4,300,"rojo","blanco","verde", "25/03/1922", "indefinido", "jupiter", "humano");
 
-        characters.add(luke);
-        characters.add(darthMaul);
-        characters.add(darthVader);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try (FileInputStream inputStream = new FileInputStream("src/main/resources/static/starwars.json")) {
+            this.characters = objectMapper.readValue(inputStream, new TypeReference<List<Character>>() {});
+        } catch (Exception e) {
+            System.out.println("Error cargando personajes:" + e.getMessage());
+        }
+
     }
     @Override
     public List<Character> getCharacters() {
