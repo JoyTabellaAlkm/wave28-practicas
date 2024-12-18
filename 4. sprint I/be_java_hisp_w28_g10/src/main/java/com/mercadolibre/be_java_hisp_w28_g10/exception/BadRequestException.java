cd95a8cbf0d0c31f3aa5fb235480e816f5f64523
astrid21 <astridmalamud@gmail.com>
2024-12-18 17:53:57 -0300

    Sprint I: elimino para volver a cargarlo

--- 4. sprint I/be_java_hisp_w28_g10/src/main/java/com/mercadolibre/be_java_hisp_w28_g10/exception/BadRequestException.java
+++
@@ -1,7 +0,0 @@
-package com.mercadolibre.be_java_hisp_w28_g10.exception;
-
-public class BadRequestException extends RuntimeException{
-    public BadRequestException(String message) {
-        super(message);
-    }
-}
--- 4. sprint I/be_java_hisp_w28_g10/src/main/java/com/mercadolibre/be_java_hisp_w28_g10/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.mercadolibre.be_java_hisp_w28_g10.exception;
-
-public class ConflictException extends RuntimeException{
-    public ConflictException(){}
-    public ConflictException(String message) {
-        super(message);
-    }
-}
