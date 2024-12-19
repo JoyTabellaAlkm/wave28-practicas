faddc4aa0cc7d7d5b568820ead4722e8b1fb63bd
Andrés Suarique <andresuarique@gmail.com>
2024-12-19 10:06:15 -0500

    Agregado proyecto sprint descomprimido

---
+++ 4. sprint I/be_java_hisp_w28_g08-develop/src/main/java/com/mercadolibre/sprint1/dto/PostDto.java
@@ -0,0 +1,31 @@
+package com.mercadolibre.sprint1.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+	
+	@JsonProperty("post_id")
+	private int id;
+
+	@JsonProperty("user_id")
+	private int userId;
+
+	@JsonFormat(pattern = "dd-MM-yyyy")
+	private LocalDate date;
+
+	private ProductDto product;
+
+	private int category;
+
+	private double price;
+
+}
