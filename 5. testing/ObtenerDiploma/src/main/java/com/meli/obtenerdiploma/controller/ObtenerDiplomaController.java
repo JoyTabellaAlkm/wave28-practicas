d5963e267be4b5cc669a904a97215a4fb1074d0d
SantiMar00 <santimarinio2000@gmail.com>
2024-12-20 16:35:42 -0300

    add ejercio alumnos y materias

---
+++ 5. testing/ObtenerDiploma/src/main/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaController.java
@@ -0,0 +1,41 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.service.IObtenerDiplomaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ObtenerDiplomaController {
+
+    @Autowired
+    IObtenerDiplomaService service;
+
+    @PostMapping("/analyzeScores")
+    public StudentDTO analyzeScores(@Valid @RequestBody StudentDTO rq) {
+        return service.analyzeScores(rq);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+}
+
+
+
+
+
