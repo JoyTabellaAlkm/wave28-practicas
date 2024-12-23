6073f8e2b621fe2ab324556a53df4b4b06db1ba8
peyk0v <peykovn@gmail.com>
2024-12-23 18:02:56 -0300

    agrego practicas de Test de Integracion (1 test por el momento)

---
+++ 5. testing/ObtenerDiploma-II/src/test/java/com/meli/obtenerdiploma/controller/ObtenerDiplomaControllerITests.java
@@ -0,0 +1,35 @@
+package com.meli.obtenerdiploma.controller;
+
+import com.meli.obtenerdiploma.model.StudentDTO;
+import com.meli.obtenerdiploma.model.SubjectDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ObtenerDiplomaControllerITests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void analyzeScoreTest() throws Exception {
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/analyzeScores/{studentId}", 1))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        assertEquals("application/json", result.getResponse().getContentType());
+    }
+}
