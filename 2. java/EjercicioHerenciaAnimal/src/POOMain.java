d1283edccb07802c709e7273cf9515e795f65e96
Cristhian <crispito2008@hotmail.com>
2024-12-26 13:49:13 -0500

    feat: Se realizan ejercicos de Herencia con respecto a animales

---
+++ 2. java/EjercicioHerenciaAnimal/src/POOMain.java
@@ -0,0 +1,16 @@
+public class POOMain {
+    public static void main(String[] args) {
+
+        Animal perro = new Perro("Perro", "Tito");
+        perro.mostrarEspecie();
+        perro.hacerSonido();
+
+        Gato gato = new Gato("Gato", "Leonicio");
+        gato.mostrarEspecie();
+        gato.hacerSonido();
+
+        Animal animal = perro;
+        animal.mostrarEspecie();
+        animal.hacerSonido();
+    }
+}
