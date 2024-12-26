904809f04d2be039c12293c7b0739ec6c1976f32
cristhian david S. <87107172+ingcdsantamaria@users.noreply.github.com>
2024-12-26 14:13:02 -0500

    Delete 2. java/Copia de EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal directory
    fix: Se corrige error de carge de archivos en el directorio equivocado

--- 2. java/Copia de EjercicioGlobal/src/main/java/com/bootcampW22/EjercicioGlobal/exception/DuplicateVehicleException.java
+++
@@ -1,7 +0,0 @@
-package com.bootcampW22.EjercicioGlobal.exception;
-
-public class DuplicateVehicleException extends RuntimeException {
-  public DuplicateVehicleException(String message) {
-    super(message);
-  }
-}
