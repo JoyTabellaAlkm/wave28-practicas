e5c4818cc9391a54a59247bc40d39106c934df97
peyk0v <peykovn@gmail.com>
2024-12-27 12:08:05 -0300

    agrego test de integracion de obtener jugador por nombre

--- 5. testing/mundial-DemoEnVivo-Unit-Integration-Test/src/test/java/ar/com/mercadolibre/mundial/integration/controller/JugadorControllerTest.java
+++ 5. testing/mundial-DemoEnVivo-Unit-Integration-Test/src/test/java/ar/com/mercadolibre/mundial/integration/controller/JugadorControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import static javax.management.Query.value;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -42,7 +43,7 @@ public class JugadorControllerTest {
     }
 
     @Test
-    void registrarJugadorError() throws Exception{
+    void registrarJugadorError() throws Exception {
         JugadorDTO jugador = new JugadorDTO(1, "Lionel Messi", "Argentina", 91);
         String request = stringedBody(jugador);
         ResultMatcher statusNotFound = status().isNotFound();
@@ -56,7 +57,37 @@ public class JugadorControllerTest {
                 .andDo(print());
     }
 
-    String stringedBody(JugadorDTO jugador) throws JsonProcessingException {
+    @Test
+    void obtenerJugadorPorNombreOk() throws Exception {
+        String nombreABuscar = "Lionel Messi";
+        ResultMatcher statusOk = status().isOk();
+        ResultMatcher jsonResponseType = content().contentType("application/json");
+        ResultMatcher expectedName = MockMvcResultMatchers.jsonPath("$.nombre").value("Lionel Messi");
+        ResultMatcher expectedGoals = MockMvcResultMatchers.jsonPath("$.goles").value(91);
+
+        mockMvc.perform(get("/jugador").queryParam("nombre", nombreABuscar))
+                .andExpect(statusOk)
+                .andExpect(jsonResponseType)
+                .andExpect(expectedName)
+                .andExpect(expectedGoals)
+                .andDo(print());
+    }
+
+    @Test
+    void obtenerJugadorPorNombreError() throws Exception {
+        String nombreABuscar = "Leo";
+        ResultMatcher statusNotFound = status().isNotFound();
+        ResultMatcher jsonResponseType = content().contentType("application/json");
+        ResultMatcher expectedErrorMessage = MockMvcResultMatchers.jsonPath("$.message").value("Jugador no encontrado");
+
+        mockMvc.perform(get("/jugador").queryParam("nombre", nombreABuscar))
+                .andExpect(statusNotFound)
+                .andExpect(jsonResponseType)
+                .andExpect(expectedErrorMessage)
+                .andDo(print());
+    }
+
+    private String stringedBody(JugadorDTO jugador) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.writer().writeValueAsString(jugador);
     }
