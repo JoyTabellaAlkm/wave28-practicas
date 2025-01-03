b5251e33d20c2dab7da0c1750ed8ba098eb95c1f
david-aliberto <david.aliberto@outlook.com>
2025-01-03 17:05:00 -0300

    Sptint II

--- 3. spring/PracticasCodeReview/src/main/java/com/bootcampW22/EjercicioGlobal/exception/BadRequestData.java
+++
@@ -1,7 +0,0 @@
-package com.bootcampW22.EjercicioGlobal.exception;
-
-public class BadRequestData extends RuntimeException {
-    public BadRequestData(String message) {
-        super(message);
-    }
-}
---
+++ 6. sprint II/be_java_hisp_w28_g10-main/src/test/java/com/mercadolibre/be_java_hisp_w28_g10/BeJavaHispW28G10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadolibre.be_java_hisp_w28_g10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeJavaHispW28G10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
