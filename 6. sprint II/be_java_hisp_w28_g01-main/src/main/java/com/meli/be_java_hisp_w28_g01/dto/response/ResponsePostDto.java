7caa8779aa3f3204fdc2af7cd9a44cf1b2dd1133
palomasosa <palososa_19@hotmail.es>
2025-01-03 17:09:39 -0300

    Sprint 2 - Grupo 1

---
+++ 6. sprint II/be_java_hisp_w28_g01-main/src/main/java/com/meli/be_java_hisp_w28_g01/dto/response/ResponsePostDto.java
@@ -0,0 +1,28 @@
+package com.meli.be_java_hisp_w28_g01.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.meli.be_java_hisp_w28_g01.dto.request.ProductDto;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ResponsePostDto {
+    @JsonProperty("user_id")
+    int userId;
+    @JsonProperty("post_id")
+    int postId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    LocalDate date;
+    ProductDto product;
+    int category;
+    double price;
+}
---
+++ 6. sprint II/be_java_hisp_w28_g01-main/src/main/java/com/meli/be_java_hisp_w28_g01/model/User.java
@@ -0,0 +1,19 @@
+package com.meli.be_java_hisp_w28_g01.model;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+public abstract class User {
+    protected int id;
+    protected String name;
+}
