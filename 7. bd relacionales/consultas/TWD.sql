7aa21cd23443fafd8486b17567a0a4622e5a5cce
nuriarobledo <nuriarobledo0@gmail.com>
2025-01-08 17:51:57 -0300

    feat: agrego ej de consultas avanzadas parte 2

---
+++ 7. bd relacionales/consultas/TWD.sql
@@ -0,0 +1,16 @@
+CREATE TEMPORARY TABLE TWD(nombre_episodio VARCHAR(50), id_episodio INT, numero_temporada INT);
+
+INSERT INTO TWD
+SELECT ep.title, ep.id, sea.number
+FROM episodes ep
+         JOIN seasons sea
+              ON sea.id = ep.season_id
+         JOIN series ser
+              ON ser.id = sea.serie_id
+WHERE ser.title = 'The Walking Dead';
+
+SELECT * FROM TWD
+WHERE numero_temporada = 1;
+
+CREATE INDEX movie_title_idx ON movies(title);
+CREATE INDEX serie_title_idx ON series(title);
