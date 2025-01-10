e93c8a984c988ed4820f2a2a9eeac66fb4a10abd
Jorge Calderón <jorgedcalderon1989@gmail.com>
2025-01-10 17:50:33 -0300

    agrego practica de clase en vivo viernes 10/01 por la tarde

---
+++ 9.  implementacion de bases de datos/miniseries/src/main/java/com/mercadolibre/miniseries/entity/MiniSeries.java
@@ -0,0 +1,20 @@
+package com.mercadolibre.miniseries.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class MiniSeries {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String name;
+    Double rating;
+    int amount_of_awards;
+}
