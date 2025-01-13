e7f51d07e16e694ad8a8d0833753dfe8895b2da9
“rivejjj” <rivero.joaquinn@gmail.com>
2025-01-13 12:45:51 -0300

    ejercicio QA testers

---
+++ 9.  implementacion de bases de datos/qatesters/src/main/java/com/mercadolibre/qatesterrs/repository/ITestCaseRepository.java
@@ -0,0 +1,12 @@
+package com.mercadolibre.qatesterrs.repository;
+
+import com.mercadolibre.qatesterrs.entity.TestCase;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITestCaseRepository extends JpaRepository<TestCase, Long> {
+
+
+
+}
