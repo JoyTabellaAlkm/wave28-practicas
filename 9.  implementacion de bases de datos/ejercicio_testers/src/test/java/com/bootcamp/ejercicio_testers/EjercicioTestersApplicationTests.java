8fc0aae6db44b34e6e2ef7db3cf4280901898b29
fcastaldoback <fercastaldo@hotmail.com>
2025-01-13 12:28:10 -0300

    Practica CRUD con JPA

---
+++ 9.  implementacion de bases de datos/ejercicio_testers/src/test/java/com/bootcamp/ejercicio_testers/EjercicioTestersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.ejercicio_testers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioTestersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
