085e8d7e99adb29cc029eb54a60a5d8a49e6051c
davara13 <daaariaspa@unal.edu.co>
2025-01-13 06:32:27 -0500

    Ejercicio de JPA + Hibernate realizado

---
+++ 9.  implementacion de bases de datos/MiniSeries/src/main/java/com/Bootcamp/MiniSeries/model/MiniSerie.java
@@ -0,0 +1,16 @@
+package com.Bootcamp.MiniSeries.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class MiniSerie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Double rating;
+    @Column(name = "amount_of_awards")
+    private int amountOfAwards;
+}
