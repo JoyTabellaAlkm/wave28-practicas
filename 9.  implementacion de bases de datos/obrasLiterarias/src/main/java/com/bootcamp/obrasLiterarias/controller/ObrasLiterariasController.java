9c94714f89cad32c6a59d9606d1556387e56fa0a
davara13 <daaariaspa@unal.edu.co>
2025-01-14 16:14:00 -0500

    cambios en el route de la api

--- 9.  implementacion de bases de datos/obrasLiterarias/src/main/java/com/bootcamp/obrasLiterarias/controller/ObrasLiterariasController.java
+++ 9.  implementacion de bases de datos/obrasLiterarias/src/main/java/com/bootcamp/obrasLiterarias/controller/ObrasLiterariasController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/obra")
+@RequestMapping("/obraliteraria")
 public class ObrasLiterariasController {
     @Autowired
     private IObraLiterariaService obraLiterariaService;
