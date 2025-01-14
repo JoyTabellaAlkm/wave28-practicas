a028acb5b7ce2a0a97fb49e7ad5ea51e954ede5c
david-aliberto <david.aliberto@outlook.com>
2025-01-14 17:06:12 -0300

    Project update

---
+++ 9.  implementacion de bases de datos/siniestrosApiHQL/src/main/java/com/siniestrosapi/siniestrosapihql/SiniestrosApiHqlApplication.java
@@ -0,0 +1,13 @@
+package com.siniestrosapi.siniestrosapihql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiniestrosApiHqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SiniestrosApiHqlApplication.class, args);
+    }
+
+}
